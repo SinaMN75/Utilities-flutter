2cf767563483a0e7b0a0f7542e0dea5f620fa6b0
Farzad <104139314+farzaddev65@users.noreply.github.com>
2023-05-17 00:48:33 +0330

    clean code 4

--- lib/components/others.dart
+++ lib/components/others.dart
@@ -8,7 +8,7 @@ Widget divider({double? width, double height = 0.6, Color color = Colors.grey, E
       margin: padding,
       width: width != null ? width : screenWidth,
       height: height,
-      color: color,
+      color: color,//
     );
 
 Widget dashedDivider({double? height}) => Row(
