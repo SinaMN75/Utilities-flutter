9b7bb369c37dee3e71d51020085ef85ae018d7d0
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2023-05-20 16:20:42 +0330

    add address

--- lib/data/dto/address.dart
+++ lib/data/dto/address.dart
@@ -57,3 +57,51 @@ class AddressReadDto {
   final bool? isDefault;
   final bool? isSelected;
 }
+
+class AddressCreateUpdateDto {
+  AddressCreateUpdateDto({
+    this.id,
+    this.receiverFullName,
+    this.receiverPhoneNumber,
+    this.address,
+    this.pelak,
+    this.unit,
+    this.postalCode,
+    this.isDefault,
+  });
+
+  factory AddressCreateUpdateDto.fromJson(final String str) => AddressCreateUpdateDto.fromMap(json.decode(str));
+
+  factory AddressCreateUpdateDto.fromMap(final Map<String, dynamic> json) => AddressCreateUpdateDto(
+    id: json["id"],
+    receiverFullName: json["receiverFullName"],
+    receiverPhoneNumber: json["receiverPhoneNumber"],
+    address: json["address"],
+    pelak: json["pelak"],
+    unit: json["unit"],
+    postalCode: json["postalCode"],
+    isDefault: json["isDefault"],
+  );
+
+  String toJson() => json.encode(toMap());
+
+  Map<String, dynamic> toMap() => <String, dynamic>{
+    "id": id,
+    "receiverFullName": receiverFullName,
+    "receiverPhoneNumber": receiverPhoneNumber,
+    "address": address,
+    "pelak": pelak,
+    "unit": unit,
+    "postalCode": postalCode,
+    "isDefault": isDefault,
+  };
+
+  final String? id;
+  final String? receiverFullName;
+  final String? receiverPhoneNumber;
+  final String? address;
+  final String? pelak;
+  final String? unit;
+  final String? postalCode;
+  final bool? isDefault;
+}
--- lib/data/remote_datasource/address_datasource.dart
+++ lib/data/remote_datasource/address_datasource.dart
@@ -9,13 +9,53 @@ class AddressDataSource {
   final String baseUrl;
 
   Future<void> readMyAddresses({
-    required final String url,
     required final Function(GenericResponse<AddressReadDto>) onResponse,
     required final Function(GenericResponse<dynamic> response) onError,
     final Function(String error)? failure,
   }) async =>
       httpGet(
         url: "$baseUrl/address/ReadMyAddresses",
+        action: (final Response<dynamic> response) => onResponse(GenericResponse<AddressReadDto>.fromJson(response.data, fromMap: AddressReadDto.fromMap)),
+        error: (final Response<dynamic> response) => onError(GenericResponse<dynamic>.fromJson(response.data)),
+        failure: failure,
+      );
+
+  Future<void> create({
+    required final AddressCreateUpdateDto dto,
+    required final Function(GenericResponse<AddressReadDto>) onResponse,
+    required final Function(GenericResponse<dynamic> response) onError,
+    final Function(String error)? failure,
+  }) async =>
+      httpPost(
+        url: "$baseUrl/address",
+        body: dto,
+        action: (final Response<dynamic> response) => onResponse(GenericResponse<AddressReadDto>.fromJson(response.data, fromMap: AddressReadDto.fromMap)),
+        error: (final Response<dynamic> response) => onError(GenericResponse<dynamic>.fromJson(response.data)),
+        failure: failure,
+      );
+
+  Future<void> update({
+    required final AddressCreateUpdateDto dto,
+    required final Function(GenericResponse<AddressReadDto>) onResponse,
+    required final Function(GenericResponse<dynamic> response) onError,
+    final Function(String error)? failure,
+  }) async =>
+      httpPut(
+        url: "$baseUrl/address",
+        body: dto,
+        action: (final Response<dynamic> response) => onResponse(GenericResponse<AddressReadDto>.fromJson(response.data, fromMap: AddressReadDto.fromMap)),
+        error: (final Response<dynamic> response) => onError(GenericResponse<dynamic>.fromJson(response.data)),
+        failure: failure,
+      );
+
+  Future<void> delete({
+    required final String id,
+    required final Function(GenericResponse<AddressReadDto>) onResponse,
+    required final Function(GenericResponse<dynamic> response) onError,
+    final Function(String error)? failure,
+  }) async =>
+      httpDelete(
+        url: "$baseUrl/address/$id",
         action: (final Response<dynamic> response) => onResponse(GenericResponse<AddressReadDto>.fromJson(response.data, fromMap: AddressReadDto.fromMap)),
         error: (final Response<dynamic> response) => onError(GenericResponse<dynamic>.fromJson(response.data)),
         failure: failure,
