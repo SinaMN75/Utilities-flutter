5d3ba2661c89a1c035357465da6aaa0974d743be
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2023-05-22 14:42:35 +0330

    :)

--- lib/view_models/key_value_view_model.dart
+++ lib/view_models/key_value_view_model.dart
@@ -1,5 +1,23 @@
+import 'package:utilities/utilities.dart';
+
 class KeyValueViewModel {
   const KeyValueViewModel(this.key, this.value, {this.id});
+
+  factory KeyValueViewModel.fromJson(final String str) => KeyValueViewModel.fromMap(json.decode(str));
+
+  factory KeyValueViewModel.fromMap(final Map<String, dynamic> json) => KeyValueViewModel(
+        json["key"],
+        json["value"],
+        id: json["id"],
+      );
+
+  String toJson() => json.encode(toMap());
+
+  Map<String, dynamic> toMap() => <String, dynamic>{
+        "id": id,
+        "key": key,
+        "value": value,
+      };
 
   final String? id;
   final String key;
