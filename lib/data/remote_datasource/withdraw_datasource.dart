2d5d0b9d668c5b4b4730751643ec0a9d2b19f62f
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2023-05-23 16:53:06 +0330

    add withdraw

--- lib/data/remote_datasource/withdraw_datasource.dart
+++ lib/data/remote_datasource/withdraw_datasource.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:utilities/data/dto/generic_response.dart';
+import 'package:utilities/data/dto/withdraw.dart';
 import 'package:utilities/utils/dio_interceptor.dart';
 
 class WithdrawDataSource {
@@ -9,15 +10,16 @@ class WithdrawDataSource {
   final String baseUrl;
 
   Future<void> withdraw({
-    required final String amount,
-    required final Function(GenericResponse) onResponse,
-    required final Function(GenericResponse response) onError,
+    required final WithdrawCreateUpdateDto dto,
+    required final Function(GenericResponse<dynamic>) onResponse,
+    required final Function(GenericResponse<dynamic> response) onError,
     final Function(String error)? failure,
   }) async =>
       httpPost(
         url: "$baseUrl/withdraw/WalletWithdrawal",
-        action: (final Response response) => onResponse(GenericResponse<dynamic>.fromJson(response.data)),
-        error: (final Response response) => onError(GenericResponse<dynamic>.fromJson(response.data)),
+        body: dto,
+        action: (final Response<dynamic> response) => onResponse(GenericResponse<dynamic>.fromJson(response.data)),
+        error: (final Response<dynamic> response) => onError(GenericResponse<dynamic>.fromJson(response.data)),
         failure: failure,
       );
 
