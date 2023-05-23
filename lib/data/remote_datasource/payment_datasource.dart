564335f8cc199cd0ce87963ad4c5e64c91752f6c
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2023-05-23 15:54:30 +0330

    fix payment

--- lib/data/remote_datasource/payment_datasource.dart
+++ lib/data/remote_datasource/payment_datasource.dart
@@ -18,7 +18,7 @@ class PaymentDataSource {
     final Function(String error)? failure,
   }) async =>
       httpGet(
-        url: "${baseUrl}Payment/IncreaseWalletBalance/$amount",
+        url: "$baseUrl/Payment/IncreaseWalletBalance/$amount",
         headers: <String, String>{
           "Authorization": "${getString(UtilitiesConstants.token)}",
         },
@@ -34,7 +34,7 @@ class PaymentDataSource {
     final Function(String error)? failure,
   }) async =>
       httpGet(
-        url: "${baseUrl}Payment/BuyProduct/$productId",
+        url: "$baseUrl/Payment/BuyProduct/$productId",
         headers: <String, String>{
           "Authorization": "${getString(UtilitiesConstants.token)}",
         },
