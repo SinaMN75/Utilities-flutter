881a1179824a2a1c042b8ae71348dc17e300b531
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2023-06-04 23:23:14 +0330

    :)

--- lib/data/remote_datasource/promote_datasource.dart
+++ lib/data/remote_datasource/promote_datasource.dart
@@ -1,5 +1,4 @@
 import 'package:dio/dio.dart';
-import 'package:utilities/data/dto/address.dart';
 import 'package:utilities/data/dto/generic_response.dart';
 import 'package:utilities/data/dto/promote.dart';
 import 'package:utilities/utils/dio_interceptor.dart';
@@ -11,27 +10,27 @@ class PromoteDataSource {
 
   Future<void> readById({
     required final String id,
-    required final Function(GenericResponse<AddressReadDto>) onResponse,
+    required final Function(GenericResponse<PromoteReadDto>) onResponse,
     required final Function(GenericResponse<dynamic> response) onError,
     final Function(String error)? failure,
   }) async =>
       httpGet(
         url: "$baseUrl/promotion/$id",
-        action: (final Response<dynamic> response) => onResponse(GenericResponse<AddressReadDto>.fromJson(response.data, fromMap: AddressReadDto.fromMap)),
+        action: (final Response<dynamic> response) => onResponse(GenericResponse<PromoteReadDto>.fromJson(response.data, fromMap: PromoteReadDto.fromMap)),
         error: (final Response<dynamic> response) => onError(GenericResponse<dynamic>.fromJson(response.data)),
         failure: failure,
       );
 
   Future<void> create({
     required final PromoteCreateUpdateDto dto,
-    required final Function(GenericResponse<AddressReadDto>) onResponse,
+    required final Function(GenericResponse<dynamic> response) onResponse,
     required final Function(GenericResponse<dynamic> response) onError,
     final Function(String error)? failure,
   }) async =>
       httpPost(
         url: "$baseUrl/promotion",
         body: dto,
-        action: (final Response<dynamic> response) => onResponse(GenericResponse<AddressReadDto>.fromJson(response.data, fromMap: AddressReadDto.fromMap)),
+        action: (final Response<dynamic> response) => onResponse(GenericResponse.fromJson(response.data)),
         error: (final Response<dynamic> response) => onError(GenericResponse<dynamic>.fromJson(response.data)),
         failure: failure,
       );
