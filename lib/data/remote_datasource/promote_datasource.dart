6bce4180ee24deca5ce89c1da58c55a954fa79b5
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2023-06-04 23:20:35 +0330

    :)

--- lib/data/remote_datasource/promote_datasource.dart
+++ lib/data/remote_datasource/promote_datasource.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:utilities/data/dto/address.dart';
 import 'package:utilities/data/dto/generic_response.dart';
+import 'package:utilities/data/dto/promote.dart';
 import 'package:utilities/utils/dio_interceptor.dart';
 
 class PromoteDataSource {
@@ -8,26 +9,27 @@ class PromoteDataSource {
 
   final String baseUrl;
 
-  Future<void> read({
+  Future<void> readById({
+    required final String id,
     required final Function(GenericResponse<AddressReadDto>) onResponse,
     required final Function(GenericResponse<dynamic> response) onError,
     final Function(String error)? failure,
   }) async =>
       httpGet(
-        url: "$baseUrl/address/ReadMyAddresses",
+        url: "$baseUrl/promotion/$id",
         action: (final Response<dynamic> response) => onResponse(GenericResponse<AddressReadDto>.fromJson(response.data, fromMap: AddressReadDto.fromMap)),
         error: (final Response<dynamic> response) => onError(GenericResponse<dynamic>.fromJson(response.data)),
         failure: failure,
       );
 
   Future<void> create({
-    required final AddressCreateUpdateDto dto,
+    required final PromoteCreateUpdateDto dto,
     required final Function(GenericResponse<AddressReadDto>) onResponse,
     required final Function(GenericResponse<dynamic> response) onError,
     final Function(String error)? failure,
   }) async =>
       httpPost(
-        url: "$baseUrl/address",
+        url: "$baseUrl/promotion",
         body: dto,
         action: (final Response<dynamic> response) => onResponse(GenericResponse<AddressReadDto>.fromJson(response.data, fromMap: AddressReadDto.fromMap)),
         error: (final Response<dynamic> response) => onError(GenericResponse<dynamic>.fromJson(response.data)),
