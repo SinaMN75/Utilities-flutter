8b2ea30ac85ab4b80ce648451ba8f9fe31e3557e
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2023-06-17 11:05:28 +0330

    :(

--- lib/utils/screenshot.dart
+++ lib/utils/screenshot.dart
@@ -1,9 +1,21 @@
+import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:utilities/utilities.dart';
 
-Future<Uint8List> screenshot({required final Widget widget}) async {
-  final ScreenshotController screenshotController = ScreenshotController();
-  return await screenshotController.captureFromWidget(widget);
-}
+Future<Uint8List> screenshot({required final Widget widget}) async => await ScreenshotController().captureFromWidget(widget);
+
+void shareWidget({
+  required final Widget widget,
+}) async =>
+    await ScreenshotController().capture(delay: const Duration(milliseconds: 10)).then((
+      final Uint8List? image,
+    ) async {
+      if (image != null) {
+        final Directory directory = await getApplicationDocumentsDirectory();
+        final File imagePath = await File('${directory.path}/image.png').create();
+        await imagePath.writeAsBytes(image);
+        shareFile(<String>[imagePath.path], "");
+      }
+    });
