8900175836bd824d585d1f3c19fbefec70d9cffd
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2023-07-31 16:29:32 +0330

    add local auth

---
+++ lib/utils/local_auth.dart
@@ -0,0 +1,24 @@
+import 'package:utilities/utilities.dart';
+
+final LocalAuthentication auth = LocalAuthentication();
+
+Future<bool> canAuthenticate() async => await auth.canCheckBiometrics || await auth.isDeviceSupported();
+
+Future<bool> authenticate({
+  final String localizedReason = "",
+  final bool useErrorDialogs = true,
+  final bool stickyAuth = false,
+  final bool sensitiveTransaction = true,
+  final bool biometricOnly = false,
+}) async {
+  final bool didAuthenticate = await auth.authenticate(
+    localizedReason: localizedReason,
+    options: AuthenticationOptions(
+      biometricOnly: biometricOnly,
+      sensitiveTransaction: sensitiveTransaction,
+      stickyAuth: stickyAuth,
+      useErrorDialogs: useErrorDialogs,
+    ),
+  );
+  return didAuthenticate;
+}
