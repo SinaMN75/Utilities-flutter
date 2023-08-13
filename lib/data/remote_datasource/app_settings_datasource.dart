9bb2682305a1c9340d8393bd287332924e40c13d
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2023-08-13 19:43:10 +0330

    :(

--- lib/data/remote_datasource/app_settings_datasource.dart
+++ lib/data/remote_datasource/app_settings_datasource.dart
@@ -5,33 +5,31 @@ import 'package:utilities/data/dto/location.dart';
 import 'package:utilities/utils/dio_interceptor.dart';
 
 class AppSettingsDataSource {
-  final String baseUrl;
-
   AppSettingsDataSource({required this.baseUrl});
 
+  final String baseUrl;
+
   Future<void> readAppSettings({
-    required final String url,
     required final Function(GenericResponse<AppSettingsDto> response) onResponse,
-    required final Function(GenericResponse errorResponse) onError,
+    required final Function(GenericResponse<dynamic> errorResponse) onError,
     final Function(String error)? failure,
   }) async =>
       httpGet(
         url: "$baseUrl/AppSettings",
-        action: (Response<dynamic> response) => onResponse(GenericResponse<AppSettingsDto>.fromJson(response.data, fromMap: AppSettingsDto.fromMap)),
-        error: (Response response) => onError(GenericResponse.fromJson(response.data, fromMap: AppSettingsDto.fromMap)),
+        action: (final Response<dynamic> response) => onResponse(GenericResponse<AppSettingsDto>.fromJson(response.data, fromMap: AppSettingsDto.fromMap)),
+        error: (final Response<dynamic> response) => onError(GenericResponse<dynamic>.fromJson(response.data, fromMap: AppSettingsDto.fromMap)),
         failure: failure,
       );
 
   Future<void> readLocation({
-    required final String url,
     required final Function(GenericResponse<LocationReadDto> response) onResponse,
-    required final Function(GenericResponse errorResponse) onError,
+    required final Function(GenericResponse<dynamic> errorResponse) onError,
     final Function(String error)? failure,
   }) async =>
       httpGet(
         url: "$baseUrl/AppSettings/ReadLocation",
-        action: (Response response) => onResponse(GenericResponse<LocationReadDto>.fromJson(response.data, fromMap: LocationReadDto.fromMap)),
-        error: (Response response) => onError(GenericResponse.fromJson(response.data, fromMap: AppSettingsDto.fromMap)),
+        action: (final Response<dynamic> response) => onResponse(GenericResponse<LocationReadDto>.fromJson(response.data, fromMap: LocationReadDto.fromMap)),
+        error: (final Response<dynamic> response) => onError(GenericResponse<dynamic>.fromJson(response.data, fromMap: AppSettingsDto.fromMap)),
         failure: failure,
       );
 }
