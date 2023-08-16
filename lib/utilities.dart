594010f949eb001cf50a8cea6c1ff8f9a612bc49
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2023-08-16 21:25:50 +0330

    :)

--- lib/utilities.dart
+++ lib/utilities.dart
@@ -16,7 +16,6 @@ export 'package:image_cropper/image_cropper.dart';
 export 'package:just_audio/just_audio.dart';
 export 'package:local_auth/local_auth.dart';
 export 'package:location/location.dart';
-export 'package:lottie/lottie.dart';
 export 'package:mobile_scanner/mobile_scanner.dart';
 export 'package:path_provider/path_provider.dart';
 export 'package:persian_datetime_picker/persian_datetime_picker.dart';
