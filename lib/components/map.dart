451696242c1879d695e96c027413a62da2c20318
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2023-08-17 12:38:00 +0330

    :)

--- lib/components/map.dart
+++ lib/components/map.dart
@@ -36,9 +36,9 @@ Widget map({
 Widget sfMap({
   final MapTileLayerController? controller,
   final MapZoomPanBehavior? zoomPanBehavior,
-  final List<UtilitiesMapMarker>? markers,
+  final List<MapMarker>? markers,
 }) {
-  final List<UtilitiesMapMarker> _markers = markers ?? <UtilitiesMapMarker>[];
+  final List<MapMarker> _markers = markers ?? <MapMarker>[];
   final MapZoomPanBehavior _zoomPanBehavior = zoomPanBehavior ??
       MapZoomPanBehavior(
         minZoomLevel: 5,
@@ -54,17 +54,13 @@ Widget sfMap({
         initialZoomLevel: 10,
         initialMarkersCount: _markers.length,
         tooltipSettings: const MapTooltipSettings(color: Colors.white),
-        markerTooltipBuilder: (final BuildContext context, final int index) {
-          final UtilitiesMapMarker i = _markers[index];
-          return i.tooltip ?? Text(i.title ?? "");
-        },
         markerBuilder: (final BuildContext context, final int index) {
-          final UtilitiesMapMarker i = _markers[index];
+          final MapMarker i = _markers[index];
           return MapMarker(
             latitude: i.latitude,
             longitude: i.longitude,
-            size: Size(i.width, i.height),
-            child: i.marker,
+            size: i.size,
+            child: i.child,
           );
         },
         urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
@@ -74,28 +70,3 @@ Widget sfMap({
   );
 }
 
-class UtilitiesMapMarker {
-  UtilitiesMapMarker({
-    required this.latitude,
-    required this.longitude,
-    required this.marker,
-    this.tooltip,
-    this.width = 20,
-    this.height = 20,
-    this.imageUrl,
-    this.title,
-    this.subtitle,
-    this.description,
-  });
-
-  final double latitude;
-  final double longitude;
-  final double width;
-  final double height;
-  final Widget marker;
-  final Widget? tooltip;
-  final String? imageUrl;
-  final String? title;
-  final String? subtitle;
-  final String? description;
-}
