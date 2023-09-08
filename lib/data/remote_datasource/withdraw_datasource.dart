a529058eba1fd1b4c32967976c4fc7c2ccaeb941
Farzad <104139314+farzaddev65@users.noreply.github.com>
2023-09-08 22:22:48 +0330

    withdraw.dart

--- lib/data/remote_datasource/withdraw_datasource.dart
+++ lib/data/remote_datasource/withdraw_datasource.dart
@@ -21,4 +21,20 @@ class WithdrawDataSource {
         error: (final Response<dynamic> response) => onError(GenericResponse<dynamic>.fromJson(response.data)),
         failure: failure,
       );
+
+// Future<void> filter({
+//   required final int withdrawState,
+//   required final Function(GenericResponse<WithdrawReadDto> response) onResponse,
+//   required final Function(GenericResponse errorResponse) onError,
+//   final Function(String error)? failure,
+// }) async =>
+//     httpPost(
+//       url: "$baseUrl/ProductV2/Filter",
+//       body: Map<String,dynamic>{"withdrawState":withdrawState},
+//       encodeBody: false,
+//       action: (Response response) => onResponse(GenericResponse<WithdrawReadDto>.fromJson(response.data, fromMap: WithdrawReadDto.fromMap)),
+//       error: (Response response) => onError(GenericResponse.fromJson(response.data)),
+//     );
+
+
 }
