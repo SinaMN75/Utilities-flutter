758bfb8d469ca7ac386dd7034125ef54448ad507
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2023-09-20 16:25:32 +0330

    :)

--- lib/data/dto/dashboard_data.dart
+++ lib/data/dto/dashboard_data.dart
@@ -2,41 +2,6 @@ import 'package:utilities/utilities.dart';
 
 class DashboardDataReadDto {
   DashboardDataReadDto({
-    this.status,
-    this.pageSize,
-    this.pageCount,
-    this.totalCount,
-    this.result,
-  });
-
-  factory DashboardDataReadDto.fromJson(final String str) => DashboardDataReadDto.fromMap(json.decode(str));
-
-  factory DashboardDataReadDto.fromMap(final Map<String, dynamic> json) => DashboardDataReadDto(
-        status: json["status"],
-        pageSize: json["pageSize"],
-        pageCount: json["pageCount"],
-        totalCount: json["totalCount"],
-        result: json["result"] == null ? null : Result.fromMap(json["result"]),
-      );
-  final int? status;
-  final int? pageSize;
-  final int? pageCount;
-  final int? totalCount;
-  final Result? result;
-
-  String toJson() => json.encode(toMap());
-
-  Map<String, dynamic> toMap() => {
-        "status": status,
-        "pageSize": pageSize,
-        "pageCount": pageCount,
-        "totalCount": totalCount,
-        "result": result?.toMap(),
-      };
-}
-
-class Result {
-  Result({
     this.categories,
     this.products,
     this.users,
@@ -45,16 +10,16 @@ class Result {
     this.transactions,
   });
 
-  factory Result.fromJson(final String str) => Result.fromMap(json.decode(str));
+  factory DashboardDataReadDto.fromJson(final String str) => DashboardDataReadDto.fromMap(json.decode(str));
 
-  factory Result.fromMap(final Map<String, dynamic> json) => Result(
-        categories: json["categories"],
-        products: json["products"],
-        users: json["users"],
-        orders: json["orders"],
-        media: json["media"],
-        transactions: json["transactions"],
-      );
+  factory DashboardDataReadDto.fromMap(final Map<String, dynamic> json) => DashboardDataReadDto(
+    categories: json["categories"],
+    products: json["products"],
+    users: json["users"],
+    orders: json["orders"],
+    media: json["media"],
+    transactions: json["transactions"],
+  );
   final int? categories;
   final int? products;
   final int? users;
@@ -65,11 +30,11 @@ class Result {
   String toJson() => json.encode(toMap());
 
   Map<String, dynamic> toMap() => {
-        "categories": categories,
-        "products": products,
-        "users": users,
-        "orders": orders,
-        "media": media,
-        "transactions": transactions,
-      };
+    "categories": categories,
+    "products": products,
+    "users": users,
+    "orders": orders,
+    "media": media,
+    "transactions": transactions,
+  };
 }
