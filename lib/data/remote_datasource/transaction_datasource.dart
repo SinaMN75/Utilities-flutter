3f5fdbfe48cc3ac67a2aa6616f05bf2a48f5303b
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2023-09-23 17:02:42 +0330

    :)

--- lib/data/remote_datasource/transaction_datasource.dart
+++ lib/data/remote_datasource/transaction_datasource.dart
@@ -1,10 +1,8 @@
-import 'package:dio/dio.dart';
+import 'package:get/get.dart';
 import 'package:utilities/data/dto/generic_response.dart';
 import 'package:utilities/data/dto/transaction.dart';
-import 'package:utilities/utils/constants.dart';
-import 'package:utilities/utils/dio_interceptor.dart';
+import 'package:utilities/utils/http_interceptor.dart';
 
-import '../../utils/local_storage.dart';
 
 class TransactionDataSource {
   final String baseUrl;
@@ -18,11 +16,8 @@ class TransactionDataSource {
   }) async =>
       httpGet(
         url: "$baseUrl/Transaction",
-        headers: <String, String>{
-          "Authorization": "${getString(UtilitiesConstants.token)}",
-        },
-        action: (Response response) => onResponse(GenericResponse<TransactionReadDto>.fromJson(response.data, fromMap: TransactionReadDto.fromMap)),
-        error: (Response response) => onError(GenericResponse.fromJson(response.data)),
+        action: (Response response) => onResponse(GenericResponse<TransactionReadDto>.fromJson(response.body, fromMap: TransactionReadDto.fromMap)),
+        error: (Response response) => onError(GenericResponse.fromJson(response.body)),
         failure: failure,
       );
 }
