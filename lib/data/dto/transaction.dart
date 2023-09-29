6dc5aecd72c5fa852875fec2fb0aa52f3b81b815
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2023-09-29 18:11:12 +0330

    :)

--- lib/data/dto/transaction.dart
+++ lib/data/dto/transaction.dart
@@ -10,6 +10,9 @@ class TransactionReadDto {
     this.paymentId,
     this.createdAt,
     this.updatedAt,
+    this.user,
+    this.order,
+    this.orderId,
   });
 
   final String? id;
@@ -20,6 +23,9 @@ class TransactionReadDto {
   final String? paymentId;
   final String? createdAt;
   final String? updatedAt;
+  final UserReadDto? user;
+  final OrderReadDto? order;
+  final String? orderId;
 
   factory TransactionReadDto.fromJson(String str) => TransactionReadDto.fromMap(json.decode(str));
 
@@ -33,7 +39,9 @@ class TransactionReadDto {
         statusId: json["statusId"],
         paymentId: json["paymentId"],
         createdAt: json["createdAt"],
-        updatedAt: json["updatedAt"],
+        orderId: json["orderId"],
+        user: json["user"] == null ? null : UserReadDto.fromMap(json["user"]),
+        order: json["order"] == null ? null : OrderReadDto.fromMap(json["order"]),
       );
 
   dynamic toMap() => {
@@ -45,5 +53,61 @@ class TransactionReadDto {
         "paymentId": paymentId,
         "createdAt": createdAt,
         "updatedAt": updatedAt,
+      };
+}
+
+class TransactionFilterDto {
+  final int? amount;
+  final String? authority;
+  final String? gatewayName;
+  final String? paymentId;
+  final String? shebaNumber;
+  final int? refId;
+  final int? statusId;
+  final int? transactionType;
+  final String? userId;
+  final String? orderId;
+
+  TransactionFilterDto({
+    this.amount,
+    this.authority,
+    this.gatewayName,
+    this.paymentId,
+    this.shebaNumber,
+    this.refId,
+    this.statusId,
+    this.transactionType,
+    this.userId,
+    this.orderId,
+  });
+
+  factory TransactionFilterDto.fromJson(String str) => TransactionFilterDto.fromMap(json.decode(str));
+
+  String toJson() => json.encode(toMap());
+
+  factory TransactionFilterDto.fromMap(Map<String, dynamic> json) => TransactionFilterDto(
+        amount: json["amount"],
+        authority: json["authority"],
+        gatewayName: json["gatewayName"],
+        paymentId: json["paymentId"],
+        shebaNumber: json["shebaNumber"],
+        refId: json["refId"],
+        statusId: json["statusId"],
+        transactionType: json["transactionType"],
+        userId: json["userId"],
+        orderId: json["orderId"],
+      );
+
+  Map<String, dynamic> toMap() => {
+        "amount": amount,
+        "authority": authority,
+        "gatewayName": gatewayName,
+        "paymentId": paymentId,
+        "shebaNumber": shebaNumber,
+        "refId": refId,
+        "statusId": statusId,
+        "transactionType": transactionType,
+        "userId": userId,
+        "orderId": orderId,
       };
 }
--- lib/data/remote_datasource/transaction_datasource.dart
+++ lib/data/remote_datasource/transaction_datasource.dart
@@ -16,4 +16,16 @@ class TransactionDataSource {
         error: (Response response) => onError(GenericResponse.fromJson(response.body)),
         failure: failure,
       );
+
+  Future<void> filter({
+    required final Function(GenericResponse<TransactionReadDto> response) onResponse,
+    required final Function(GenericResponse errorResponse) onError,
+    final Function(String error)? failure,
+  }) async =>
+      httpGet(
+        url: "$baseUrl/Transaction",
+        action: (Response response) => onResponse(GenericResponse<TransactionReadDto>.fromJson(response.body, fromMap: TransactionReadDto.fromMap)),
+        error: (Response response) => onError(GenericResponse.fromJson(response.body)),
+        failure: failure,
+      );
 }
