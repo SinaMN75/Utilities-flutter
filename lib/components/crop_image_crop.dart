a4aa0bbc4309806fe5c58a6aa4d0e2454c64a91b
Farzad <104139314+farzaddev65@users.noreply.github.com>
2023-10-18 15:54:10 +0330

    add customWebImageCropper

--- lib/components/crop_image_crop.dart
+++ lib/components/crop_image_crop.dart
@@ -1,6 +1,62 @@
 part of 'components.dart';
 
+
 Widget customImageCropper({
+  required final Function(List<CroppedFile> cropFiles) result,
+  final int maxImages = 5,
+}) {
+  final RxList<CroppedFile> cropperFiles = <CroppedFile>[].obs;
+  Widget _items({required final CroppedFile param, required final int index}) => Stack(
+    alignment: Alignment.bottomLeft,
+    children: <Widget>[
+      imageFile(File(param.path), width: 128, height: 128),
+      const Icon(
+        Icons.close_outlined,
+        size: 32,
+        color: Colors.white,
+      ).container(width: 32, height: 32, backgroundColor: Colors.red, radius: 50).onTap(() {
+        cropperFiles.removeAt(index);
+        result(cropperFiles);
+      }),
+    ],
+  ).marginSymmetric(horizontal: 4);
+
+  return SizedBox(
+    height: 110,
+    child: SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Obx(
+            () => Row(
+          children: <Widget>[
+            Row(
+              children: cropperFiles.mapIndexed((final int index, final CroppedFile item) => _items(param: cropperFiles[index], index: index)).toList(),
+            ),
+            const SizedBox(width: 8),
+            if (cropperFiles.length < maxImages)
+              Icon(Icons.add, size: 60, color: context.theme.dividerColor)
+                  .container(
+                radius: 10,
+                borderColor: context.theme.dividerColor,
+                width: 100,
+                height: 100,
+              )
+                  .onTap(
+                    () => cropImageCrop(
+                  aspectRatio: const CropAspectRatio(ratioX: 1, ratioY: 1),
+                  result: (final CroppedFile cropped) {
+                    cropperFiles.add(cropped);
+                    result(cropperFiles);
+                  },
+                ),
+              ),
+          ],
+        ),
+      ),
+    ),
+  );
+}
+
+Widget customWebImageCropper({
   required final Function(List<CroppedFile> cropFiles) result,
   final int maxImages = 5,
 }) {
@@ -59,13 +115,14 @@ Future<void> cropImageCrop({
   final int? compressQuality,
   final int? boundaryWidth,
   final int? boundaryHeight,
+  final CropAspectRatio? aspectRatio,
   final WebTranslations? webTranslations,
 }) async {
   final XFile? pickedFile = await ImagePicker().pickImage(source: ImageSource.gallery);
   if (pickedFile != null) {
     final CroppedFile? croppedFile = await ImageCropper().cropImage(
       sourcePath: pickedFile.path,
-      aspectRatio: const CropAspectRatio(ratioX: 9, ratioY: 16),
+      aspectRatio:aspectRatio?? const CropAspectRatio(ratioX: 9, ratioY: 16),
       uiSettings: <PlatformUiSettings>[
         WebUiSettings(
           context: context,
