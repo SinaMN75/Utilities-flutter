a694f756de9475766417e604c66ac60f5897a025
Farzad <104139314+farzaddev65@users.noreply.github.com>
2023-11-02 18:33:34 +0330

    add enums_extension.dart

--- lib/utils/extensions/enums_extension.dart
+++ lib/utils/extensions/enums_extension.dart
@@ -1,10 +1,10 @@
 part of '../utils.dart';
 
 extension TagProductsExtentions<T> on List<TagProduct> {
-  TagProduct? getByNumber(int number) => this.where((element) => element.number == number).toList().firstOrNull;
-  TagProduct? getByTitle(String title) => this.where((element) => element.title == title || element.titleTr1 == title).toList().firstOrNull;
+  TagProduct? getByNumber(final int number) => where((final TagProduct element) => element.number == number).toList().firstOrNull;
+  TagProduct? getByTitle(final String title) => where((final TagProduct element) => element.title == title || element.titleTr1 == title).toList().firstOrNull;
 }
 
 extension TagProductExtentions<T> on TagProduct {
-  String getTitle() => (Get.locale == const Locale("fa") ? this.title : this.titleTr1);
+  String getTitle() => (Get.locale == const Locale("fa") ? title : titleTr1);
 }
