a87093d9b1a76065a873ac5f1b561d9dab3458cb
Farzad <104139314+farzaddev65@users.noreply.github.com>
2023-11-03 16:38:11 +0330

    add enums_extension.dart

--- lib/utils/extensions/enums_extension.dart
+++ lib/utils/extensions/enums_extension.dart
@@ -2,7 +2,8 @@ part of '../utils.dart';
 
 extension TagProductsExtentions<T> on List<TagProduct> {
   TagProduct? getByNumber(final int number) => where((final TagProduct element) => element.number == number).toList().firstOrNull;
-  TagProduct? getByTitle(final String title) => where((final TagProduct element) => element.title == title || element.titleTr1 == title).toList().firstOrNull;
+  TagProduct? getByNumbers(final List<int> tags) => where((final TagProduct element) => tags.contains(element.number)).toList().firstOrNull;
+  TagProduct? getByTitle(final String title) => where((final TagProduct tagProduct) => tagProduct.title == title || tagProduct.titleTr1 == title).toList().firstOrNull;
 }
 
 extension TagProductExtentions<T> on TagProduct {
