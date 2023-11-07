ec89e1acc987403c161a70e5fa643d7065e18f48
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2023-11-07 12:38:32 +0330

    :)

---
+++ lib/utils/persian_fonts.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+
+class PersianFonts extends TextStyle {
+  static const Vazir = TextStyle(fontFamily: 'Vazir');
+  static const Samim = TextStyle(fontFamily: 'Samim');
+  static const Shabnam = TextStyle(fontFamily: 'Shabnam');
+  static const Sahel = TextStyle(fontFamily: 'Sahel');
+  static const Yekan = TextStyle(fontFamily: 'Yekan');
+  static const IranSansFaNum = TextStyle(fontFamily: 'IranSansFaNum');
+
+  static TextTheme vazirTextTheme = TextTheme(
+    displayLarge: PersianFonts.Vazir,
+    displayMedium: PersianFonts.Vazir,
+    displaySmall: PersianFonts.Vazir,
+    headlineLarge: PersianFonts.Vazir,
+    headlineMedium: PersianFonts.Vazir,
+    headlineSmall: PersianFonts.Vazir,
+    titleLarge: PersianFonts.Vazir,
+    titleMedium: PersianFonts.Vazir,
+    titleSmall: PersianFonts.Vazir,
+    bodyLarge: PersianFonts.Vazir,
+    bodyMedium: PersianFonts.Vazir,
+    bodySmall: PersianFonts.Vazir,
+    labelLarge: PersianFonts.Vazir,
+    labelMedium: PersianFonts.Vazir,
+    labelSmall: PersianFonts.Vazir,
+  );
+
+  static TextTheme samimTextTheme = TextTheme(
+    displayLarge: PersianFonts.Samim,
+    displayMedium: PersianFonts.Samim,
+    displaySmall: PersianFonts.Samim,
+    headlineLarge: PersianFonts.Samim,
+    headlineMedium: PersianFonts.Samim,
+    headlineSmall: PersianFonts.Samim,
+    titleLarge: PersianFonts.Samim,
+    titleMedium: PersianFonts.Samim,
+    titleSmall: PersianFonts.Samim,
+    bodyLarge: PersianFonts.Samim,
+    bodyMedium: PersianFonts.Samim,
+    bodySmall: PersianFonts.Samim,
+    labelLarge: PersianFonts.Samim,
+    labelMedium: PersianFonts.Samim,
+    labelSmall: PersianFonts.Samim,
+  );
+
+  static TextTheme shabnamTextTheme = TextTheme(
+    displayLarge: PersianFonts.Shabnam,
+    displayMedium: PersianFonts.Shabnam,
+    displaySmall: PersianFonts.Shabnam,
+    headlineLarge: PersianFonts.Shabnam,
+    headlineMedium: PersianFonts.Shabnam,
+    headlineSmall: PersianFonts.Shabnam,
+    titleLarge: PersianFonts.Shabnam,
+    titleMedium: PersianFonts.Shabnam,
+    titleSmall: PersianFonts.Shabnam,
+    bodyLarge: PersianFonts.Shabnam,
+    bodyMedium: PersianFonts.Shabnam,
+    bodySmall: PersianFonts.Shabnam,
+    labelLarge: PersianFonts.Shabnam,
+    labelMedium: PersianFonts.Shabnam,
+    labelSmall: PersianFonts.Shabnam,
+  );
+
+  static TextTheme sahelTextTheme = TextTheme(
+    displayLarge: PersianFonts.Sahel,
+    displayMedium: PersianFonts.Sahel,
+    displaySmall: PersianFonts.Sahel,
+    headlineLarge: PersianFonts.Sahel,
+    headlineMedium: PersianFonts.Sahel,
+    headlineSmall: PersianFonts.Sahel,
+    titleLarge: PersianFonts.Sahel,
+    titleMedium: PersianFonts.Sahel,
+    titleSmall: PersianFonts.Sahel,
+    bodyLarge: PersianFonts.Sahel,
+    bodyMedium: PersianFonts.Sahel,
+    bodySmall: PersianFonts.Sahel,
+    labelLarge: PersianFonts.Sahel,
+    labelMedium: PersianFonts.Sahel,
+    labelSmall: PersianFonts.Sahel,
+  );
+
+  static TextTheme yekanTextTheme = TextTheme(
+    displayLarge: PersianFonts.Yekan,
+    displayMedium: PersianFonts.Yekan,
+    displaySmall: PersianFonts.Yekan,
+    headlineLarge: PersianFonts.Yekan,
+    headlineMedium: PersianFonts.Yekan,
+    headlineSmall: PersianFonts.Yekan,
+    titleLarge: PersianFonts.Yekan,
+    titleMedium: PersianFonts.Yekan,
+    titleSmall: PersianFonts.Yekan,
+    bodyLarge: PersianFonts.Yekan,
+    bodyMedium: PersianFonts.Yekan,
+    bodySmall: PersianFonts.Yekan,
+    labelLarge: PersianFonts.Yekan,
+    labelMedium: PersianFonts.Yekan,
+    labelSmall: PersianFonts.Yekan,
+  );
+  
+  static TextTheme iranSansFaNumTextTheme = TextTheme(
+    displayLarge: PersianFonts.IranSansFaNum,
+    displayMedium: PersianFonts.IranSansFaNum,
+    displaySmall: PersianFonts.IranSansFaNum,
+    headlineLarge: PersianFonts.IranSansFaNum,
+    headlineMedium: PersianFonts.IranSansFaNum,
+    headlineSmall: PersianFonts.IranSansFaNum,
+    titleLarge: PersianFonts.IranSansFaNum,
+    titleMedium: PersianFonts.IranSansFaNum,
+    titleSmall: PersianFonts.IranSansFaNum,
+    bodyLarge: PersianFonts.IranSansFaNum,
+    bodyMedium: PersianFonts.IranSansFaNum,
+    bodySmall: PersianFonts.IranSansFaNum,
+    labelLarge: PersianFonts.IranSansFaNum,
+    labelMedium: PersianFonts.IranSansFaNum,
+    labelSmall: PersianFonts.IranSansFaNum,
+  );
+}
