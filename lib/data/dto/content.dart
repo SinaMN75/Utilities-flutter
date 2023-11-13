1c95db15ad366a1ad7c7fbc7a4a53a7fb832dc94
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2023-11-13 20:43:21 +0330

    :)

--- lib/data/dto/content.dart
+++ lib/data/dto/content.dart
@@ -127,12 +127,38 @@ class ContentCreateUpdateDto {
 }
 
 class ContentJsonDetail {
-  String? dribble;
-  String? website;
+  final String? instagram;
+  final String? telegram;
+  final String? whatsApp;
+  final String? linkedIn;
+  final String? dribble;
+  final String? soundCloud;
+  final String? pinterest;
+  final String? website;
+  final String? phoneNumber1;
+  final String? phoneNumber2;
+  final String? email1;
+  final String? email2;
+  final String? address1;
+  final String? address2;
+  final String? address3;
 
   ContentJsonDetail({
+    this.instagram,
+    this.telegram,
+    this.whatsApp,
+    this.linkedIn,
     this.dribble,
+    this.soundCloud,
+    this.pinterest,
     this.website,
+    this.phoneNumber1,
+    this.phoneNumber2,
+    this.email1,
+    this.email2,
+    this.address1,
+    this.address2,
+    this.address3,
   });
 
   factory ContentJsonDetail.fromJson(String str) => ContentJsonDetail.fromMap(json.decode(str));
@@ -140,12 +166,39 @@ class ContentJsonDetail {
   String toJson() => json.encode(removeNullEntries(toMap()));
 
   factory ContentJsonDetail.fromMap(Map<String, dynamic> json) => ContentJsonDetail(
+        instagram: json["instagram"],
+        telegram: json["telegram"],
+        whatsApp: json["whatsApp"],
+        linkedIn: json["linkedIn"],
         dribble: json["dribble"],
+        soundCloud: json["soundCloud"],
+        pinterest: json["pinterest"],
         website: json["website"],
+        phoneNumber1: json["phoneNumber1"],
+        phoneNumber2: json["phoneNumber2"],
+        email1: json["email1"],
+        email2: json["email2"],
+        address1: json["address1"],
+        address2: json["address2"],
+        address3: json["address3"],
       );
 
-  Map<String, dynamic> toMap() => {
+  Map<String, dynamic> toMap() =>
+      {
+        "instagram": instagram,
+        "telegram": telegram,
+        "whatsApp": whatsApp,
+        "linkedIn": linkedIn,
         "dribble": dribble,
+        "soundCloud": soundCloud,
+        "pinterest": pinterest,
         "website": website,
+        "phoneNumber1": phoneNumber1,
+        "phoneNumber2": phoneNumber2,
+        "email1": email1,
+        "email2": email2,
+        "address1": address1,
+        "address2": address2,
+        "address3": address3,
       };
 }
