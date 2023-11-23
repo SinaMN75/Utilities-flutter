73a49180982928df57373c985d1ce7596fc4d31e
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2023-11-23 21:27:18 +0330

    :)

--- lib/utilities.dart
+++ lib/utilities.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:utilities/utilities.dart';
 
 export 'dart:async';
@@ -5,6 +6,10 @@ export 'dart:convert';
 
 export 'package:cached_network_image/cached_network_image.dart';
 export 'package:file_picker/file_picker.dart';
+export 'package:firebase_analytics/firebase_analytics.dart';
+export 'package:firebase_auth/firebase_auth.dart';
+export 'package:firebase_crashlytics/firebase_crashlytics.dart';
+export 'package:firebase_messaging/firebase_messaging.dart';
 export 'package:flutter/material.dart';
 export 'package:flutter_colorpicker/flutter_colorpicker.dart';
 export 'package:flutter_contacts/contact.dart';
@@ -13,6 +18,7 @@ export 'package:flutter_map/flutter_map.dart';
 export 'package:flutter_svg/flutter_svg.dart';
 export 'package:get/get.dart';
 export 'package:get_storage/get_storage.dart';
+export 'package:google_sign_in/google_sign_in.dart';
 export 'package:group_button/group_button.dart';
 export 'package:image_cropper/image_cropper.dart';
 export 'package:just_audio/just_audio.dart';
@@ -31,11 +37,6 @@ export 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 export 'package:url_launcher/url_launcher.dart';
 export 'package:video_box/video_box.dart';
 export 'package:webviewx_plus/webviewx_plus.dart';
-export 'package:firebase_auth/firebase_auth.dart';
-export 'package:firebase_messaging/firebase_messaging.dart';
-export 'package:firebase_analytics/firebase_analytics.dart';
-export 'package:firebase_crashlytics/firebase_crashlytics.dart';
-export 'package:google_sign_in/google_sign_in.dart';
 
 export 'components/components.dart';
 export 'components/fdottedline.dart';
@@ -45,7 +46,8 @@ export 'utils/excel_to_json.dart';
 export 'utils/utils.dart';
 export 'view_models/view_models.dart';
 
-void initUtilities() async {
+void initUtilities({FirebaseOptions? firebaseOptions}) async {
   WidgetsFlutterBinding.ensureInitialized();
   await GetStorage.init();
+  if (firebaseOptions != null) await Firebase.initializeApp(options: firebaseOptions);
 }
