72339dcf1faf33d235761adc8e77af79c3c607b5
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2023-12-09 11:24:10 +0330

    :)

--- lib/data/remote_datasource/mail_sms_notification_datasource.dart
+++ lib/data/remote_datasource/mail_sms_notification_datasource.dart
@@ -14,22 +14,29 @@ class MailSmsNotificationDataSource {
       httpPost(
         url: "$baseUrl/MailSmsNotification/SendNotification",
         body: dto,
-        action: (Response response) => onResponse(GenericResponse<String>.fromJson(response.body, fromMap: CreateMailSmsNotificationDto.fromMap)),
-        error: (Response response) => onError(GenericResponse.fromJson(response.body)),
+        action: (Response response) => onResponse(
+            GenericResponse<String>.fromJson(response.body,
+                fromMap: CreateMailSmsNotificationDto.fromMap)),
+        error: (Response response) =>
+            onError(GenericResponse.fromJson(response.body)),
         failure: failure,
       );
 
   Future<void> sendOtpSms({
     required final SendOtpSmsDto dto,
-    required final Function(GenericResponse response) onResponse,
-    required final Function(GenericResponse errorResponse) onError,
+    final Function(GenericResponse response)? onResponse,
+    final Function(GenericResponse errorResponse)? onError,
     final Function(String error)? failure,
   }) async =>
       httpPost(
         url: "$baseUrl/MailSmsNotification/SendOtpSms",
         body: dto,
-        action: (Response response) => onResponse(GenericResponse<String>.fromJson(response.body)),
-        error: (Response response) => onError(GenericResponse.fromJson(response.body)),
+        action: (Response response) => onResponse == null
+            ? () {}
+            : onResponse(GenericResponse<String>.fromJson(response.body)),
+        error: (Response response) => onError == null
+            ? () {}
+            : onError(GenericResponse.fromJson(response.body)),
         failure: failure,
       );
 }
