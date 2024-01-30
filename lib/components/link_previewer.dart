9db1ceee2b8e8db3e7885f2fa6cc5085ba6ae1d6
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2024-01-30 23:53:56 +0330

    :)

--- lib/components/link_previewer.dart
+++ lib/components/link_previewer.dart
@@ -1,28 +1,26 @@
 part of 'components.dart';
 
-Widget linkPreviewer() => AnyLinkPreview(
-      link: "https://vardaan.app/",
+Widget linkPreviewer({required final String link}) => AnyLinkPreview(
+      link: link,
       displayDirection: UIDirection.uiDirectionHorizontal,
-      showMultimedia: false,
-      bodyMaxLines: 5,
-      bodyTextOverflow: TextOverflow.ellipsis,
-      titleStyle: TextStyle(
+      bodyMaxLines: 10,
+      titleStyle: const TextStyle(
         color: Colors.black,
         fontWeight: FontWeight.bold,
         fontSize: 15,
       ),
-      bodyStyle: TextStyle(color: Colors.grey, fontSize: 12),
+      bodyStyle: const TextStyle(color: Colors.grey, fontSize: 12),
       errorBody: 'Show my custom error body',
       errorTitle: 'Show my custom error title',
       errorWidget: Container(
         color: Colors.grey[300],
-        child: Text('Oops!'),
+        child: const Text('Oops!'),
       ),
       errorImage: "https://google.com/",
-      cache: Duration(days: 7),
+      cache: const Duration(days: 7),
       backgroundColor: Colors.grey[300],
       borderRadius: 12,
-      removeElevation: false,
-      boxShadow: [BoxShadow(blurRadius: 3, color: Colors.grey)],
+      urlLaunchMode: LaunchMode.externalApplication,
+      boxShadow: const <BoxShadow>[BoxShadow(blurRadius: 3, color: Colors.grey)],
       onTap: () {}, // This disables tap event
     );
