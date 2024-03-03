feadd0abb12fba431f6499932b31aaf5bf484930
Farzad <104139314+farzaddev65@users.noreply.github.com>
2024-03-04 00:49:33 +0330

    update main

--- lib/utilities.dart
+++ lib/utilities.dart
@@ -38,7 +38,6 @@ export 'package:screenshot/screenshot.dart';
 export 'package:share_plus/share_plus.dart';
 export 'package:syncfusion_flutter_barcodes/barcodes.dart';
 export 'package:syncfusion_flutter_charts/charts.dart';
-export 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 export 'package:url_launcher/url_launcher.dart';
 export 'package:video_player/video_player.dart';
 export 'package:webviewx_plus/webviewx_plus.dart';
