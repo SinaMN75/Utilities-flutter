d74d286c31e0c4261e75a11174e01fc792b3d86f
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2024-03-06 11:56:20 +0330

    :)

--- lib/utils/file.dart
+++ lib/utils/file.dart
@@ -394,7 +394,7 @@ Widget filePickerList({
                             .map(
                               (final FileData e) => e
                                 ..parentId = parentId
-                                ..id = generateUUID(),
+                                ..id = parentId == null ? null : generateUUID(),
                             )
                             .toList(),
                       );
