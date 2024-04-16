48f34a6e9e9404d08c96d7a948aaed40471497eb
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2024-04-17 00:19:00 +0330

    :)

--- lib/components/link_previewer.dart
+++ lib/components/link_previewer.dart
@@ -4,8 +4,21 @@ Widget linkPreviewer({required final String link}) => AnyLinkPreview(
       link: link,
       displayDirection: UIDirection.uiDirectionHorizontal,
       bodyMaxLines: 10,
-      bodyStyle: const TextStyle(fontSize: 12),
+      titleStyle: const TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 15),
+      errorWidget: Text(
+        link,
+        style: TextStyle(
+          fontSize: 12,
+          color: context.isDarkMode ? Colors.white : Colors.black,
+        ),
+      ).onTap(() => launchURL(link)),
+      bodyStyle: const TextStyle(
+        fontSize: 12,
+        color: Colors.black,
+      ),
+      cache: const Duration(days: 7),
       borderRadius: 12,
+      backgroundColor: Colors.white,
       urlLaunchMode: LaunchMode.externalApplication,
       boxShadow: const <BoxShadow>[BoxShadow(blurRadius: 3)],
     );
