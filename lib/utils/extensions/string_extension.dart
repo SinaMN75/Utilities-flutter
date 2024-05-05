eb3e8818334b2657d8d9474fb5ce59d6b50411af
Farzad <104139314+farzaddev65@users.noreply.github.com>
2024-05-05 14:13:56 +0330

    update 4

--- lib/utils/extensions/string_extension.dart
+++ lib/utils/extensions/string_extension.dart
@@ -95,47 +95,49 @@ extension StringExtensions on String {
 
   String toTimeAgo({final bool numericDates = false, final bool persian = false}) {
     try {
-      final Duration difference = DateTime.now().difference(intl.DateFormat("yyyy-MM-ddThh:mm:sss").parse(this));
+      debugPrint(this);//
+      final Duration difference = DateTime.now().difference(DateTime.parse(this.replaceAll('Z', '')));
+      debugPrint(this);//
       if (difference.inDays > 8)
         return this.substring(0, 10);
       else if ((difference.inDays / 7).floor() >= 1)
         return persian
             ? numericDates
-                ? '۱ هفته پیش'
-                : 'هفته پیش'
+            ? '۱ هفته پیش'
+            : 'هفته پیش'
             : numericDates
-                ? '1 week ago'
-                : 'Last week';
+            ? '1 week ago'
+            : 'Last week';
       else if (difference.inDays >= 2)
         return persian ? '${difference.inDays.toString().persianNumber()} روز پیش' : '${difference.inDays} days ago';
       else if (difference.inDays >= 1)
         return persian
             ? numericDates
-                ? '۱ روز پیش'
-                : 'دیروز'
+            ? '۱ روز پیش'
+            : 'دیروز'
             : numericDates
-                ? '1 day ago'
-                : 'Yesterday';
+            ? '1 day ago'
+            : 'Yesterday';
       else if (difference.inHours >= 2)
         return persian ? '${difference.inHours.toString().persianNumber()} ساعت پیش' : '${difference.inHours} hours ago';
       else if (difference.inHours >= 1)
         return persian
             ? numericDates
-                ? '۱ ساعت پیش'
-                : 'یک ساعت پیش'
+            ? '۱ ساعت پیش'
+            : 'یک ساعت پیش'
             : numericDates
-                ? '1 hour ago'
-                : 'An hour ago';
+            ? '1 hour ago'
+            : 'An hour ago';
       else if (difference.inMinutes >= 2)
         return persian ? '${difference.inMinutes.toString().persianNumber()} دقیقه پیش' : '${difference.inMinutes} minutes ago';
       else if (difference.inMinutes >= 1)
         return persian
             ? numericDates
-                ? '۱ دقیقه پیش'
-                : 'یک دقیقه پیش'
+            ? '۱ دقیقه پیش'
+            : 'یک دقیقه پیش'//
             : numericDates
-                ? '1 minute ago'
-                : 'A minute ago';
+            ? '1 minute ago'
+            : 'A minute ago';
       else if (difference.inSeconds >= 3)
         return persian ? '${difference.inSeconds.toString().persianNumber()} ثانیه پیش' : '${difference.inSeconds} seconds ago';
       else
