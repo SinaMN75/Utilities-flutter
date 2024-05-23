2031bd2a37512221d0948656189a8860e49e876a
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2024-05-23 18:50:07 +0330

    :)

--- lib/utilities2.dart
+++ lib/utilities2.dart
@@ -1,6 +1,7 @@
 export 'dart:developer';
 export 'dart:io';
 
+export 'package:flutter_multi_formatter/flutter_multi_formatter.dart';
 export 'package:latlong2/latlong.dart';
 export 'package:pin_code_fields/pin_code_fields.dart';
 export 'package:pull_to_refresh/pull_to_refresh.dart';
