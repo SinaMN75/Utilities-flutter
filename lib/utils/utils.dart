619e2fbfb8f17af1af234326660e35dcfeabbf1d
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2024-05-23 15:14:17 +0330

    :)

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -65,9 +65,14 @@ String getPrice(final int i) => intl.NumberFormat('###,###,###,###,000').format(
 
 bool hasMatch(final String? value, final String pattern) => (value == null) ? false : RegExp(pattern).hasMatch(value);
 
-void logout({required final VoidCallback onLoggedOut}) => showYesCancelDialog(
-  title: "خروج از سیستم",
-      description: "آیا از خروج از سیستم اطمینان دارید؟",
+void logout({
+  required final VoidCallback onLoggedOut,
+  final String title = "خروج از سیستم",
+  final String description = "آیا از خروج از سیستم اطمینان دارید؟",
+}) =>
+    showYesCancelDialog(
+      title: title,
+      description: description,
       onYesButtonTap: onLoggedOut,
     );
 
@@ -123,7 +128,8 @@ void showYesCancelDialog({
   required final String description,
   required final VoidCallback onYesButtonTap,
   final VoidCallback? onCancelButtonTap,
-  final String? yesButtonTitle,
+  final String? yesButtonTitle = "بله",
+  final String? cancelButtonTitle = 'انصراف',
 }) =>
     showDialog(
       context: context,
@@ -135,17 +141,17 @@ void showYesCancelDialog({
         actions: <Widget>[
           SizedBox(
               child: button(
-            width: screenWidth / 4,
+                width: screenWidth / 4,
             backgroundColor: context.theme.primaryColorDark,
             onTap: onCancelButtonTap ?? back,
-            title: 'انصراف',
+            title: cancelButtonTitle,
             textStyle: context.textTheme.bodyMedium,
           )),
           SizedBox(
               child: button(
-            width: screenWidth / 4,
+                width: screenWidth / 4,
             onTap: onYesButtonTap,
-            title: yesButtonTitle ?? "بله",
+            title: yesButtonTitle,
             textStyle: context.textTheme.bodyMedium,
           )),
         ],
