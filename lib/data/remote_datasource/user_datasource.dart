840d80f6f2785b147fa33de58c31505dc780d403
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2024-05-23 14:19:26 +0330

    :)

--- lib/data/remote_datasource/user_datasource.dart
+++ lib/data/remote_datasource/user_datasource.dart
@@ -149,14 +149,14 @@ class UserDataSource {
 
   void loginWithEmail({
     required final LoginWithEmail dto,
-    required final Function(GenericResponse<String> response) onResponse,
+    required final Function(GenericResponse<UserReadDto> response) onResponse,
     required final Function(GenericResponse errorResponse) onError,
     final Function(dynamic error)? failure,
   }) =>
       httpPost(
         url: "$baseUrl/user/LoginWithEmail",
         body: dto,
-        action: (final Response response) => onResponse(GenericResponse<String>.fromJson(response.body, fromMap: UserReadDto.fromMap)),
+        action: (final Response response) => onResponse(GenericResponse<UserReadDto>.fromJson(response.body, fromMap: UserReadDto.fromMap)),
         error: (final Response response) => onError(GenericResponse.fromJson(response.body)),
       );
 
