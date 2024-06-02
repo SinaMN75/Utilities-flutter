769c6e699f0ff6ff6ad8d5c83bdedd6b4136e8ca
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2024-06-02 21:24:46 +0330

    :)

--- lib/components/barcode_qrcode.dart
+++ lib/components/barcode_qrcode.dart
@@ -31,27 +31,3 @@ Widget barcode(
     showValue: showValue,
   );
 }
-
-///https://pub.dev/packages/mobile_scanner
-///
-///iOS:
-/// <key>NSCameraUsageDescription</key>
-/// <string>This app needs camera access to scan QR codes</string>
-/// <key>NSPhotoLibraryUsageDescription</key>
-/// <string>This app needs photos access to get QR code from photo library</string>
-
-Widget qrScanner({
-  required final Function(String? value) onResponse,
-  final MobileScannerController? controller,
-}) =>
-    MobileScanner(
-      controller: controller,
-      onDetect: (final BarcodeCapture capture) {
-        for (final Barcode barcode in capture.barcodes) onResponse(barcode.rawValue);
-      },
-    );
-
-Widget qrcodeScannerPage({required final Function(String? value) onResponse}) => scaffold(
-      appBar: AppBar(),
-      body: qrScanner(onResponse: onResponse),
-    );
---
+++ lib/documents.dart
@@ -0,0 +1,25 @@
+// mobile_scanner: ^5.1.1
+
+///https://pub.dev/packages/mobile_scanner
+///
+///iOS:
+/// <key>NSCameraUsageDescription</key>
+/// <string>This app needs camera access to scan QR codes</string>
+/// <key>NSPhotoLibraryUsageDescription</key>
+/// <string>This app needs photos access to get QR code from photo library</string>
+
+// Widget qrScanner({
+//   required final Function(String? value) onResponse,
+//   final MobileScannerController? controller,
+// }) =>
+//     MobileScanner(
+//       controller: controller,
+//       onDetect: (final BarcodeCapture capture) {
+//         for (final Barcode barcode in capture.barcodes) onResponse(barcode.rawValue);
+//       },
+//     );
+//
+// Widget qrcodeScannerPage({required final Function(String? value) onResponse}) => scaffold(
+//       appBar: AppBar(),
+//       body: qrScanner(onResponse: onResponse),
+//     );
