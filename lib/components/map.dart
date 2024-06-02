f1aafd11ea246c9de22f88bbc4fb7e9e9801219c
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2024-06-02 21:43:59 +0330

    :)

--- lib/components/map.dart
+++ lib/components/map.dart
@@ -12,23 +12,28 @@ Widget map({
   final bool zoomButtons = true,
   final Function(TapPosition tapPosition, LatLng point)? onTap,
   final Function(TapPosition tapPosition, LatLng point)? onLongPress,
-  final Function(MapPosition position, bool hasGesture)? onPositionChanged,
+  final Function(MapCamera position, bool hasGesture)? onPositionChanged,
   final Function(PointerUpEvent event, LatLng point)? onPointerUp,
 }) =>
-    FlutterMap(
-      mapController: controller,
-      options: MapOptions(
-        center: center,
-        zoom: zoom,
-        onTap: onTap,
-        maxZoom: maxZoom,
-        minZoom: minZoom,
-        onLongPress: onLongPress,
-        enableMultiFingerGestureRace: true,
-        onPositionChanged: onPositionChanged,
-        onPointerUp: onPointerUp,
-      ),
-      nonRotatedChildren: <Widget>[
+    Stack(
+      children: <Widget>[
+        FlutterMap(
+          mapController: controller,
+          options: MapOptions(
+            initialCenter: center,
+            initialZoom: zoom,
+            onTap: onTap,
+            maxZoom: maxZoom,
+            minZoom: minZoom,
+            onLongPress: onLongPress,
+            onPositionChanged: onPositionChanged,
+            onPointerUp: onPointerUp,
+          ),
+          children: <Widget>[
+            TileLayer(urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'),
+            MarkerLayer(markers: markers ?? <Marker>[]),
+          ],
+        ),
         if (currentLocationButton)
           FloatingActionButton(
             mini: true,
@@ -47,18 +52,14 @@ Widget map({
             leading: FloatingActionButton(
               mini: true,
               child: const Icon(Icons.add),
-              onPressed: () => controller.move(controller.center, controller.zoom + 1),
+              onPressed: () => controller.move(controller.camera.center, controller.camera.zoom + 1),
             ),
             trailing: FloatingActionButton(
               mini: true,
               child: const Icon(Icons.remove),
-              onPressed: () => controller.move(controller.center, controller.zoom - 1),
+              onPressed: () => controller.move(controller.camera.center, controller.camera.zoom - 1),
             ),
           ).paddingAll(16).alignAtBottomRight(),
         if (centerWidget != null) Align(child: centerWidget),
-      ],
-      children: <Widget>[
-        TileLayer(urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'),
-        MarkerLayer(markers: markers ?? <Marker>[]),
       ],
     );
