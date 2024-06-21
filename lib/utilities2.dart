91d497f40461ff2e172b9d53d2380ae988fbecec
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2024-06-22 00:05:53 +0330

    :)

--- lib/utilities2.dart
+++ lib/utilities2.dart
@@ -5,3 +5,4 @@ export 'package:flutter_multi_formatter/flutter_multi_formatter.dart';
 export 'package:latlong2/latlong.dart';
 export 'package:pin_code_fields/pin_code_fields.dart';
 export 'package:pull_to_refresh/pull_to_refresh.dart';
+export 'package:geolocator/geolocator.dart';
