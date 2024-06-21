53c9c0ac45a6635579a097f5580489c6e3e63b8b
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2024-06-21 23:28:54 +0330

    :)

--- lib/components/map.dart
+++ lib/components/map.dart
@@ -1,65 +1,98 @@
 part of 'components.dart';
 
-Widget map({
-  required final MapController controller,
-  final LatLng center = const LatLng(35, 55),
-  final double zoom = 10,
-  final double minZoom = 5,
-  final double maxZoom = 18,
-  final List<Marker>? markers,
-  final Widget? centerWidget,
-  final bool currentLocationButton = true,
-  final bool zoomButtons = true,
-  final Function(TapPosition tapPosition, LatLng point)? onTap,
-  final Function(TapPosition tapPosition, LatLng point)? onLongPress,
-  final Function(MapCamera position, bool hasGesture)? onPositionChanged,
-  final Function(PointerUpEvent event, LatLng point)? onPointerUp,
-}) =>
-    Stack(
-      children: <Widget>[
-        FlutterMap(
-          mapController: controller,
-          options: MapOptions(
-            initialCenter: center,
-            initialZoom: zoom,
-            onTap: onTap,
-            maxZoom: maxZoom,
-            minZoom: minZoom,
-            onLongPress: onLongPress,
-            onPositionChanged: onPositionChanged,
-            onPointerUp: onPointerUp,
+class UMap extends StatelessWidget {
+  const UMap({
+    super.key,
+    required this.controller,
+    this.center = const LatLng(35, 55),
+    this.zoom = 10,
+    this.minZoom = 5,
+    this.maxZoom = 18,
+    this.markers,
+    this.centerWidget,
+    this.zoomButtons = true,
+    this.currentLocationLayer = true,
+    this.onTap,
+    this.onLongPress,
+    this.onPositionChanged,
+    this.onMyLocationTap,
+    this.onPointerUp,
+  });
+
+  final MapController controller;
+  final LatLng center;
+  final double zoom;
+  final double minZoom;
+  final double maxZoom;
+  final List<Marker>? markers;
+  final Widget? centerWidget;
+  final bool zoomButtons;
+  final bool currentLocationLayer;
+  final Function(TapPosition tapPosition, LatLng point)? onTap;
+  final Function(TapPosition tapPosition, LatLng point)? onMyLocationTap;
+  final Function(TapPosition tapPosition, LatLng point)? onLongPress;
+  final Function(MapCamera position, bool hasGesture)? onPositionChanged;
+  final Function(PointerUpEvent event, LatLng point)? onPointerUp;
+
+  @override
+  Widget build(BuildContext context) => Stack(
+        children: <Widget>[
+          FlutterMap(
+            mapController: controller,
+            options: MapOptions(
+              initialCenter: center,
+              initialZoom: zoom,
+              onTap: onTap,
+              maxZoom: maxZoom,
+              minZoom: minZoom,
+              onLongPress: onLongPress,
+              onPositionChanged: onPositionChanged,
+              onPointerUp: onPointerUp,
+            ),
+            children: <Widget>[
+              TileLayer(urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'),
+              MarkerLayer(markers: markers ?? <Marker>[]),
+              if (currentLocationLayer)
+                CurrentLocationLayer(
+                  alignDirectionOnUpdate: AlignOnUpdate.always,
+                  style: LocationMarkerStyle(
+                    marker: const DefaultLocationMarker(
+                      child: Icon(Icons.navigation, color: Colors.white),
+                    ),
+                    markerSize: const Size(40, 40),
+                    markerDirection: MarkerDirection.heading,
+                    showAccuracyCircle: true,
+                    showHeadingSector: true,
+                  ),
+                ),
+            ],
           ),
-          children: <Widget>[
-            TileLayer(urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'),
-            MarkerLayer(markers: markers ?? <Marker>[]),
-          ],
-        ),
-        if (currentLocationButton)
-          FloatingActionButton(
-            mini: true,
-            onPressed: () async {
-              try {
-                // final LocationData? location = await getLocation();
-                // controller.move(LatLng(location!.latitude!, location.longitude!), 15);
-              } catch (e) {
-                debugPrint("");
-              }
-            },
-            child: const Icon(Icons.my_location),
-          ).paddingAll(16).alignAtBottomLeft(),
-        if (zoomButtons)
-          iconTextVertical(
-            leading: FloatingActionButton(
+          if (onMyLocationTap != null)
+            FloatingActionButton(
               mini: true,
-              child: const Icon(Icons.add),
-              onPressed: () => controller.move(controller.camera.center, controller.camera.zoom + 1),
-            ),
-            trailing: FloatingActionButton(
-              mini: true,
-              child: const Icon(Icons.remove),
-              onPressed: () => controller.move(controller.camera.center, controller.camera.zoom - 1),
-            ),
-          ).paddingAll(16).alignAtBottomRight(),
-        if (centerWidget != null) Align(child: centerWidget),
-      ],
-    );
+              onPressed: () async => onMyLocationTap,
+              child: const Icon(Icons.my_location),
+            ).paddingAll(16).alignAtBottomLeft(),
+          if (zoomButtons)
+            iconTextVertical(
+              leading: FloatingActionButton(
+                mini: true,
+                child: const Icon(Icons.add),
+                onPressed: () => controller.move(
+                  controller.camera.center,
+                  controller.camera.zoom + 1,
+                ),
+              ),
+              trailing: FloatingActionButton(
+                mini: true,
+                child: const Icon(Icons.remove),
+                onPressed: () => controller.move(
+                  controller.camera.center,
+                  controller.camera.zoom - 1,
+                ),
+              ),
+            ).paddingAll(16).alignAtBottomRight(),
+          if (centerWidget != null) Align(child: centerWidget),
+        ],
+      );
+}
