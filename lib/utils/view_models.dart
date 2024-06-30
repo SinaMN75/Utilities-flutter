c2974d7f3f7f20524c092075c1bc9707c1020a93
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2024-06-30 22:01:32 +0330

    :)

--- lib/utils/view_models.dart
+++ lib/utils/view_models.dart
@@ -35,17 +35,33 @@ class MediaViewModel {
 }
 
 class KeyValueViewModel {
-  KeyValueViewModel(this.key, this.value, {this.id});
+  KeyValueViewModel(
+    this.key,
+    this.value, {
+    this.id,
+    this.description,
+  });
 
   factory KeyValueViewModel.fromJson(final String str) => KeyValueViewModel.fromMap(json.decode(str));
 
-  factory KeyValueViewModel.fromMap(final dynamic json) => KeyValueViewModel(json["key"], json["value"], id: json["id"]);
+  factory KeyValueViewModel.fromMap(final dynamic json) => KeyValueViewModel(
+        json["key"],
+        json["value"],
+        id: json["id"],
+        description: json["description"],
+      );
 
   String toJson() => json.encode(toMap());
 
-  Map<String, dynamic> toMap() => <String, dynamic>{"id": id, "key": key, "value": value};
+  Map<String, dynamic> toMap() => <String, dynamic>{
+        "id": id,
+        "key": key,
+        "value": value,
+        "description": description,
+      };
 
   String? id;
   String key;
   String value;
+  String? description;
 }
