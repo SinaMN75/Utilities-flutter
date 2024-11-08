7f1ca7fe31497b15565b324743772ef24142fc67
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2024-11-08 18:12:20 +0330

    add webview

---
+++ lib/components/webview.dart
@@ -0,0 +1,14 @@
+import 'package:utilities/utilities.dart';
+
+class UWebView extends StatelessWidget {
+  const UWebView({required this.url, super.key});
+
+  final String url;
+
+  @override
+  Widget build(BuildContext context) => WebViewWidget(
+        controller: WebViewController()
+          ..setJavaScriptMode(JavaScriptMode.unrestricted)
+          ..loadRequest(Uri.parse(url)),
+      );
+}
