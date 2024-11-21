9f28e49779e084a333dd85d28749cefc2688a8b8
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2024-11-21 18:19:24 +0330

    :)

--- lib/data/dto/question.dart
+++ lib/data/dto/question.dart
@@ -159,3 +159,151 @@ class QuestionJsonDetail {
         "answers": List<dynamic>.from(answers.map((x) => x.toMap())),
       };
 }
+
+class UserQuestionAnswerCreateDto {
+  final String userId;
+  final List<UserQuestionAnswer> userQuestionAnswer;
+
+  UserQuestionAnswerCreateDto({
+    required this.userId,
+    required this.userQuestionAnswer,
+  });
+
+  factory UserQuestionAnswerCreateDto.fromJson(String str) => UserQuestionAnswerCreateDto.fromMap(json.decode(str));
+
+  String toJson() => json.encode(toMap());
+
+  factory UserQuestionAnswerCreateDto.fromMap(Map<String, dynamic> json) => UserQuestionAnswerCreateDto(
+        userId: json["userId"],
+        userQuestionAnswer: List<UserQuestionAnswer>.from(json["userQuestionAnswer"].map((x) => UserQuestionAnswer.fromMap(x))),
+      );
+
+  Map<String, dynamic> toMap() => {
+        "userId": userId,
+        "userQuestionAnswer": List<dynamic>.from(userQuestionAnswer.map((x) => x.toMap())),
+      };
+}
+
+class UserQuestionAnswer {
+  final String question;
+  final String answer;
+  final String hint;
+  final int point;
+
+  UserQuestionAnswer({
+    required this.question,
+    required this.answer,
+    required this.hint,
+    required this.point,
+  });
+
+  factory UserQuestionAnswer.fromJson(String str) => UserQuestionAnswer.fromMap(json.decode(str));
+
+  String toJson() => json.encode(toMap());
+
+  factory UserQuestionAnswer.fromMap(Map<String, dynamic> json) => UserQuestionAnswer(
+        question: json["question"],
+        answer: json["answer"],
+        hint: json["hint"],
+        point: json["point"],
+      );
+
+  Map<String, dynamic> toMap() => {
+        "question": question,
+        "answer": answer,
+        "hint": hint,
+        "point": point,
+      };
+}
+
+class UserQuestionAnswerFilterDto {
+  final int? pageSize;
+  final int? pageNumber;
+  final String? fromDate;
+  final List<String>? userIds;
+
+  UserQuestionAnswerFilterDto({
+    this.pageSize,
+    this.pageNumber,
+    this.fromDate,
+    this.userIds,
+  });
+
+  factory UserQuestionAnswerFilterDto.fromJson(String str) => UserQuestionAnswerFilterDto.fromMap(json.decode(str));
+
+  String toJson() => json.encode(toMap());
+
+  factory UserQuestionAnswerFilterDto.fromMap(Map<String, dynamic> json) => UserQuestionAnswerFilterDto(
+        pageSize: json["pageSize"],
+        pageNumber: json["pageNumber"],
+        fromDate: json["fromDate"],
+        userIds: json["userIds"] == null ? [] : List<String>.from(json["userIds"]!.map((x) => x)),
+      );
+
+  Map<String, dynamic> toMap() => {
+        "pageSize": pageSize,
+        "pageNumber": pageNumber,
+        "fromDate": fromDate,
+        "userIds": userIds == null ? [] : List<dynamic>.from(userIds!.map((x) => x)),
+      };
+}
+
+class UserQuestionAnswerReadDto {
+  final String? id;
+  final String? createdAt;
+  final String? updatedAt;
+  final String? userId;
+  final UserReadDto? user;
+  final UserQuestionAnswerReadDtoJsonDetail? jsonDetail;
+
+  UserQuestionAnswerReadDto({
+    this.id,
+    this.createdAt,
+    this.updatedAt,
+    this.userId,
+    this.user,
+    this.jsonDetail,
+  });
+
+  factory UserQuestionAnswerReadDto.fromJson(String str) => UserQuestionAnswerReadDto.fromMap(json.decode(str));
+
+  String toJson() => json.encode(toMap());
+
+  factory UserQuestionAnswerReadDto.fromMap(Map<String, dynamic> json) => UserQuestionAnswerReadDto(
+        id: json["id"],
+        createdAt: json["createdAt"],
+        updatedAt: json["updatedAt"],
+        userId: json["userId"],
+        user: json["user"] == null ? null : UserReadDto.fromMap(json["user"]),
+        jsonDetail: json["jsonDetail"] == null ? null : UserQuestionAnswerReadDtoJsonDetail.fromMap(json["jsonDetail"]),
+      );
+
+  Map<String, dynamic> toMap() => {
+        "id": id,
+        "createdAt": createdAt,
+        "updatedAt": updatedAt,
+        "userId": userId,
+        "user": user?.toMap(),
+        "jsonDetail": jsonDetail?.toMap(),
+      };
+}
+
+class UserQuestionAnswerReadDtoJsonDetail {
+  final List<UserQuestionAnswer>? userQuestionAnswer;
+
+  UserQuestionAnswerReadDtoJsonDetail({
+    this.userQuestionAnswer,
+  });
+
+  factory UserQuestionAnswerReadDtoJsonDetail.fromJson(String str) => UserQuestionAnswerReadDtoJsonDetail.fromMap(json.decode(str));
+
+  String toJson() => json.encode(toMap());
+
+  factory UserQuestionAnswerReadDtoJsonDetail.fromMap(Map<String, dynamic> json) => UserQuestionAnswerReadDtoJsonDetail(
+        userQuestionAnswer: json["userQuestionAnswer"] == null ? [] : List<UserQuestionAnswer>.from(json["userQuestionAnswer"]!.map((x) => UserQuestionAnswer.fromMap(x))),
+      );
+
+  Map<String, dynamic> toMap() => {
+        "userQuestionAnswer": userQuestionAnswer == null ? [] : List<dynamic>.from(userQuestionAnswer!.map((x) => x.toMap())),
+      };
+}
