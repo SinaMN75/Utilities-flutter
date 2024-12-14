e992a71adac775bf999293130faa1aadaf21f325
Sina MohammadZadeh <34570827+SinaMN75@users.noreply.github.com>
2024-12-14 21:14:27 +0330

    :)

--- lib/utils/excel.dart
+++ lib/utils/excel.dart
@@ -1,28 +1,28 @@
-import 'package:syncfusion_flutter_xlsio/xlsio.dart';
-import 'package:utilities/utilities.dart';
+// import 'package:syncfusion_flutter_xlsio/xlsio.dart';
 
-class UExcelData {
-  UExcelData({required this.title, required this.data});
 
-  final List<List<String>> data;
-  final String title;
-}
-
-class UExcel {
-  Future<void> generateExcel(final UExcelData data) async {
-    final Workbook workbook = Workbook();
-
-    final Worksheet worksheet = workbook.worksheets[0];
-    worksheet.name = data.title;
-    worksheet.showGridlines = false;
-
-    data.data.forEachIndexed(
-      (final int index, final List<String> i) {
-        worksheet.getRangeByIndex(i.indexOf(i[index]), index).setText(i[index]);
-      },
-    );
-
-    final List<int> bytes = workbook.saveAsCSV(',');
-    workbook.dispose();
-  }
-}
+// class UExcelData {
+//   UExcelData({required this.title, required this.data});
+//
+//   final List<List<String>> data;
+//   final String title;
+// }
+//
+// class UExcel {
+//   Future<void> generateExcel(final UExcelData data) async {
+//     final Workbook workbook = Workbook();
+//
+//     final Worksheet worksheet = workbook.worksheets[0];
+//     worksheet.name = data.title;
+//     worksheet.showGridlines = false;
+//
+//     data.data.forEachIndexed(
+//       (final int index, final List<String> i) {
+//         worksheet.getRangeByIndex(i.indexOf(i[index]), index).setText(i[index]);
+//       },
+//     );
+//
+//     final List<int> bytes = workbook.saveAsCSV(',');
+//     workbook.dispose();
+//   }
+// }
