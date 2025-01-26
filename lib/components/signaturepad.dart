2acaaeff7865eb9880dfa4c025e617302f75fdde
Sina MohammadZadeh <sina75mn@outlook.com>
2025-01-26 15:03:25 +0330

    add SfSignaturePad

--- lib/components/signaturepad.dart
+++ lib/components/signaturepad.dart
@@ -1,13 +1,52 @@
+import 'dart:ui' as ui;
+
 import 'package:u/utilities.dart';
 
-class SignaturePad extends StatefulWidget {
-  const SignaturePad({super.key});
+class SignaturePad extends StatelessWidget {
+  SignaturePad({
+    super.key,
+    required this.onSave,
+    this.saveButtonText = "Save",
+    this.clearButtonText = "Clear",
+  });
+
+  final GlobalKey<SfSignaturePadState> signatureGlobalKey = GlobalKey();
+  final Function(FileData) onSave;
+  final String saveButtonText;
+  final String clearButtonText;
 
   @override
-  State<SignaturePad> createState() => _SignaturePadState();
-}
+  Widget build(BuildContext context) => Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            margin: EdgeInsets.all(10),
+            decoration: BoxDecoration(border: Border.all(color: Colors.grey)),
+            child: SfSignaturePad(
+              key: signatureGlobalKey,
+              backgroundColor: Colors.white,
+              strokeColor: Colors.black,
+              minimumStrokeWidth: 1,
+              maximumStrokeWidth: 4,
+            ),
+          ),
+          SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              TextButton(onPressed: handleSaveButtonPressed, child: Text(saveButtonText)),
+              TextButton(onPressed: () => signatureGlobalKey.currentState!.clear(), child: Text(clearButtonText)),
+            ],
+          )
+        ],
+      );
 
-class _SignaturePadState extends State<SignaturePad> {
-  @override
-  Widget build(BuildContext context) => SfSignaturePad();
+  void handleSaveButtonPressed() async {
+    final ui.Image data = await signatureGlobalKey.currentState!.toImage(pixelRatio: 3.0);
+    final ByteData? bytes = await data.toByteData(format: ui.ImageByteFormat.png);
+    final Uint8List byte = bytes!.buffer.asUint8List();
+    File file = await UFile.writeToFile(byte);
+    onSave(FileData(path: file.path, bytes: byte, extension: "png"));
+  }
 }
