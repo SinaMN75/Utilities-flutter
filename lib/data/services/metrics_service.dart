0c4cd57f6045fbcba01bad0a70938e2801a38ab3
Sina MohammadZadeh <sina75mn@outlook.com>
2025-05-19 15:26:42 +0330

    :)

--- lib/data/services/metrics_service.dart
+++ lib/data/services/metrics_service.dart
@@ -6,15 +6,16 @@ class MetricsService {
   final String baseUrl;
 
   void get({
-    required final Function(UResponse<MetricsResponse> r) onOk,
-    required final Function(UResponse<dynamic> e) onError,
+    required final Function(MetricsResponse r) onOk,
+    required final VoidCallback onError,
     final Function(Exception)? onException,
-  }) => UHttpClient().get(
-      "$baseUrl/metrics",
-      onSuccess: (final Response r) => onOk(UResponse<MetricsResponse>.fromJson(r.body, (final dynamic i) => MetricsResponse.fromMap(i))),
-      onError: (final Response r) => onError(UResponse<dynamic>.fromJson(r.body, (final dynamic i) => i)),
-      onException: (dynamic e) {
-        if (onException != null) onException(e);
-      },
-    );
+  }) =>
+      UHttpClient().get(
+        "$baseUrl/metrics",
+        onSuccess: (final Response r) => onOk(MetricsResponse.fromJson(r.body)),
+        onError: (final Response r) => onError(),
+        onException: (dynamic e) {
+          if (onException != null) onException(e);
+        },
+      );
 }
