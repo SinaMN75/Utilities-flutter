bad21788449178bfe666d56a5715c36206c6008a
Sina MohammadZadeh <sina75mn@outlook.com>
2025-08-09 22:56:39 +0330

    :)

--- lib/data/services/auth_service.dart
+++ lib/data/services/auth_service.dart
@@ -28,7 +28,7 @@ class AuthService {
     );
   }
 
-  void login({
+  void loginWithUserNamePassword({
     required final LoginWithUserNamePasswordParams p,
     required final Function(UResponse<LoginResponse> r) onOk,
     required final Function(UResponse<dynamic> e) onError,
@@ -36,6 +36,22 @@ class AuthService {
   }) =>
       UHttpClient().post(
         "$baseUrl/auth/LoginWithUserNamePassword",
+        body: p.toMap().add("apiKey", apiKey).add("token", token),
+        onSuccess: (final Response r) => onOk(UResponse<LoginResponse>.fromJson(r.body, (final dynamic i) => LoginResponse.fromMap(i))),
+        onError: (final Response r) => onError(UResponse<dynamic>.fromJson(r.body, (final dynamic i) => i)),
+        onException: (dynamic e) {
+          if (onException != null) onException(e);
+        },
+      );
+
+  void loginWithEmailPassword({
+    required final LoginWithEmailPasswordParams p,
+    required final Function(UResponse<LoginResponse> r) onOk,
+    required final Function(UResponse<dynamic> e) onError,
+    final Function(Exception)? onException,
+  }) =>
+      UHttpClient().post(
+        "$baseUrl/auth/LoginWithEmailPassword",
         body: p.toMap().add("apiKey", apiKey).add("token", token),
         onSuccess: (final Response r) => onOk(UResponse<LoginResponse>.fromJson(r.body, (final dynamic i) => LoginResponse.fromMap(i))),
         onError: (final Response r) => onError(UResponse<dynamic>.fromJson(r.body, (final dynamic i) => i)),
