67b5c29822a91175de7d3634906828582e882236
Sina MohammadZadeh <sina75mn@outlook.com>
2025-11-25 19:00:24 +0330

    :)

---
+++ lib/u_admin/settings_page.dart
@@ -0,0 +1,57 @@
+import "package:u/utilities.dart";
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({required this.onFinish, super.key});
+
+  final VoidCallback onFinish;
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+    final bool isDarkMode = theme.brightness == Brightness.dark;
+    return UScaffold(
+      appBar: AppBar(title: const Text("Settings")),
+      body: Column(
+        children: <Widget>[
+          settingTile(
+            icon: Icons.dark_mode_outlined,
+            title: UApp.isDarkMode ? "Dark Theme" : "Light Theme",
+            trailing: Switch(
+              value: isDarkMode,
+              onChanged: (_) {
+                UApp.switchTheme();
+                delay(100, onFinish);
+              },
+            ),
+          ).pSymmetric(vertical: 12),
+          settingTile(
+            icon: Icons.language_outlined,
+            title: "Language",
+            trailing: DropdownButton<String>(
+              value: "persian",
+              items: const <DropdownMenuItem<String>>[
+                DropdownMenuItem<String>(value: "persian", child: Text("Persian")),
+                DropdownMenuItem<String>(value: "english", child: Text("English")),
+              ],
+              onChanged: (_) {},
+            ),
+          ).pSymmetric(vertical: 12),
+          settingTile(
+            icon: Icons.person_outline,
+            title: "Update Profile",
+            onTap: () {},
+          ).pSymmetric(vertical: 12),
+          UButton(
+            onTap: () => UAdminUtils.signOut(onFinish: onFinish),
+            title: "Logout",
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget settingTile({required IconData icon, required String title, Widget? trailing, VoidCallback? onTap}) => Card(
+        elevation: 1,
+        child: ListTile(leading: Icon(icon), title: Text(title), trailing: trailing, onTap: onTap, contentPadding: const EdgeInsets.symmetric(horizontal: 16), minLeadingWidth: 24),
+      );
+}
