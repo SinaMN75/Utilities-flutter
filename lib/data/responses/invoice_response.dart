4f0a15db1cb87b692e392a87a778e64a9fba9618
Sina MohammadZadeh <sina75mn@outlook.com>
2025-11-30 19:01:30 +0330

    :)

--- lib/data/responses/invoice_response.dart
+++ lib/data/responses/invoice_response.dart
@@ -12,7 +12,7 @@ class UInvoiceResponse {
   final double? paidAmount;
   final double? penaltyAmount;
   final DateTime? paidDate;
-  final DateTime? dueDate;
+  final DateTime dueDate;
   final DateTime? nextInvoiceIssueDate;
   final String? trackingNumber;
   final UUserResponse? user;
@@ -26,13 +26,13 @@ class UInvoiceResponse {
     required this.updatedAt,
     required this.jsonData,
     required this.tags,
+    required this.dueDate,
     this.deletedAt,
     this.debtAmount,
     this.creditorAmount,
     this.paidAmount,
     this.penaltyAmount,
     this.paidDate,
-    this.dueDate,
     this.nextInvoiceIssueDate,
     this.trackingNumber,
     this.user,
@@ -46,46 +46,46 @@ class UInvoiceResponse {
   String toJson() => json.encode(toMap());
 
   factory UInvoiceResponse.fromMap(Map<String, dynamic> json) => UInvoiceResponse(
-        id: json["id"],
-        createdAt: DateTime.parse(json["createdAt"]),
-        updatedAt: DateTime.parse(json["updatedAt"]),
-        deletedAt: json["deletedAt"] == null ? null : DateTime.parse(json["deletedAt"]),
-        jsonData: UInvoiceJsonData.fromMap(json["jsonData"]),
-        tags: json["tags"] == null ? <int>[] : List<int>.from(json["tags"]!.map((dynamic x) => x)),
-        debtAmount: json["debtAmount"].toString().toDouble(),
-        creditorAmount: json["creditorAmount"].toString().toDouble(),
-        paidAmount: json["paidAmount"].toString().toDouble(),
-        penaltyAmount: json["penaltyAmount"].toString().toDouble(),
-        paidDate: json["paidDate"] == null ? null : DateTime.parse(json["paidDate"]),
-        dueDate: json["dueDate"] == null ? null : DateTime.parse(json["dueDate"]),
-        nextInvoiceIssueDate: json["nextInvoiceIssueDate"] == null ? null : DateTime.parse(json["nextInvoiceIssueDate"]),
-        trackingNumber: json["trackingNumber"],
-        user: json["user"] == null ? null : UUserResponse.fromMap(json["user"]),
-        userId: json["userId"],
-        contract: json["contract"],
-        contractId: json["contractId"],
-      );
+    id: json["id"],
+    createdAt: DateTime.parse(json["createdAt"]),
+    updatedAt: DateTime.parse(json["updatedAt"]),
+    deletedAt: json["deletedAt"] == null ? null : DateTime.parse(json["deletedAt"]),
+    jsonData: UInvoiceJsonData.fromMap(json["jsonData"]),
+    tags: json["tags"] == null ? <int>[] : List<int>.from(json["tags"]!.map((dynamic x) => x)),
+    debtAmount: json["debtAmount"].toString().toDouble(),
+    creditorAmount: json["creditorAmount"].toString().toDouble(),
+    paidAmount: json["paidAmount"].toString().toDouble(),
+    penaltyAmount: json["penaltyAmount"].toString().toDouble(),
+    paidDate: json["paidDate"] == null ? null : DateTime.parse(json["paidDate"]),
+    dueDate: DateTime.parse(json["dueDate"]),
+    nextInvoiceIssueDate: json["nextInvoiceIssueDate"] == null ? null : DateTime.parse(json["nextInvoiceIssueDate"]),
+    trackingNumber: json["trackingNumber"],
+    user: json["user"] == null ? null : UUserResponse.fromMap(json["user"]),
+    userId: json["userId"],
+    contract: json["contract"],
+    contractId: json["contractId"],
+  );
 
   Map<String, dynamic> toMap() => <String, dynamic>{
-        "id": id,
-        "createdAt": createdAt.toIso8601String(),
-        "updatedAt": updatedAt.toIso8601String(),
-        "deletedAt": deletedAt?.toIso8601String(),
-        "jsonData": jsonData.toMap(),
-        "tags": List<dynamic>.from(tags.map((int x) => x)),
-        "debtAmount": debtAmount,
-        "creditorAmount": creditorAmount,
-        "paidAmount": paidAmount,
-        "penaltyAmount": penaltyAmount,
-        "paidDate": paidDate?.toIso8601String(),
-        "dueDate": dueDate?.toIso8601String(),
-        "nextInvoiceIssueDate": nextInvoiceIssueDate?.toIso8601String(),
-        "trackingNumber": trackingNumber,
-        "user": user?.toMap(),
-        "userId": userId,
-        "contract": contract,
-        "contractId": contractId,
-      };
+    "id": id,
+    "createdAt": createdAt.toIso8601String(),
+    "updatedAt": updatedAt.toIso8601String(),
+    "deletedAt": deletedAt?.toIso8601String(),
+    "jsonData": jsonData.toMap(),
+    "tags": List<dynamic>.from(tags.map((int x) => x)),
+    "debtAmount": debtAmount,
+    "creditorAmount": creditorAmount,
+    "paidAmount": paidAmount,
+    "penaltyAmount": penaltyAmount,
+    "paidDate": paidDate?.toIso8601String(),
+    "dueDate": dueDate.toIso8601String(),
+    "nextInvoiceIssueDate": nextInvoiceIssueDate?.toIso8601String(),
+    "trackingNumber": trackingNumber,
+    "user": user?.toMap(),
+    "userId": userId,
+    "contract": contract,
+    "contractId": contractId,
+  };
 }
 
 class UInvoiceJsonData {
@@ -100,10 +100,10 @@ class UInvoiceJsonData {
   String toJson() => json.encode(toMap());
 
   factory UInvoiceJsonData.fromMap(Map<String, dynamic> json) => UInvoiceJsonData(
-        description: json["description"],
-      );
+    description: json["description"],
+  );
 
   Map<String, dynamic> toMap() => <String, dynamic>{
-        "description": description,
-      };
+    "description": description,
+  };
 }
