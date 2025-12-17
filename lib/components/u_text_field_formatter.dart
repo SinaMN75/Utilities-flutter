3cede74a69f6e9e59c2396d748ae68a2927cf91f
Sina MohammadZadeh <sina75mn@outlook.com>
2025-12-17 09:01:00 +0330

    :)

--- lib/components/u_text_field_formatter.dart
+++ lib/components/u_text_field_formatter.dart
@@ -1,4 +1,4 @@
-import "package:flutter/services.dart";
+import "package:u/utilities.dart";
 
 class UCurrencyInputFormatter extends TextInputFormatter {
   final String thousandSeparator;
@@ -8,7 +8,7 @@ class UCurrencyInputFormatter extends TextInputFormatter {
   UCurrencyInputFormatter({
     this.thousandSeparator = ",",
     this.decimalSeparator = ".",
-    this.maxDigits = 12,
+    this.maxDigits = 24,
   });
 
   @override
@@ -20,7 +20,57 @@ class UCurrencyInputFormatter extends TextInputFormatter {
       return newValue;
     }
 
-    final String cleanedText = newValue.text.replaceAll(RegExp(r"[^\d.]"), "");
+    if (newValue.text.length < oldValue.text.length) {
+      return _handleDeletion(oldValue, newValue);
+    }
+
+    return _handleInsertion(oldValue, newValue);
+  }
+
+  TextEditingValue _handleDeletion(
+    TextEditingValue oldValue,
+    TextEditingValue newValue,
+  ) {
+    final String cleanedOld = oldValue.text.toEnglishNumber().replaceAll(thousandSeparator, "").replaceAll(decimalSeparator, ".");
+    final String cleanedNew = newValue.text.toEnglishNumber().replaceAll(thousandSeparator, "").replaceAll(decimalSeparator, ".");
+
+    if (cleanedNew.length == cleanedOld.length) {
+      final int oldCursorPos = oldValue.selection.baseOffset;
+      if (oldCursorPos > 0 && oldCursorPos <= oldValue.text.length) {
+        final String deletedChar = oldValue.text[oldCursorPos - 1];
+        if (deletedChar == thousandSeparator || deletedChar == decimalSeparator) {
+          final StringBuffer newText = StringBuffer();
+          for (int i = 0; i < oldValue.text.length; i++) {
+            if (i != oldCursorPos - 2 && i != oldCursorPos - 1) {
+              newText.write(oldValue.text[i]);
+            }
+          }
+
+          final String formatted = _formatText(newText.toString());
+          return TextEditingValue(
+            text: formatted,
+            selection: TextSelection.collapsed(
+              offset: _calculateCursorPosition(formatted, oldCursorPos - 2),
+            ),
+          );
+        }
+      }
+    }
+
+    final String formatted = _formatText(newValue.text);
+    final int newCursorPos = _calculateCursorPosition(formatted, newValue.selection.baseOffset);
+
+    return TextEditingValue(
+      text: formatted,
+      selection: TextSelection.collapsed(offset: newCursorPos),
+    );
+  }
+
+  TextEditingValue _handleInsertion(
+    TextEditingValue oldValue,
+    TextEditingValue newValue,
+  ) {
+    final String cleanedText = newValue.text.toEnglishNumber().replaceAll(RegExp(r"[^\d.]"), "");
 
     final List<String> parts = cleanedText.split(".");
     String integerPart = parts[0];
@@ -50,21 +100,68 @@ class UCurrencyInputFormatter extends TextInputFormatter {
       finalText = "$formattedInteger$decimalSeparator$decimalPart";
     }
 
-    int cursorPosition = finalText.length;
-    if (oldValue.text.isNotEmpty && newValue.text.isNotEmpty) {
-      final int oldNumericCount = oldValue.text.replaceAll(RegExp(r"[^\d]"), "").length;
-      final int newNumericCount = newValue.text.replaceAll(RegExp(r"[^\d]"), "").length;
-
-      if (newNumericCount > oldNumericCount) {
-        cursorPosition = finalText.length;
-      } else if (newNumericCount < oldNumericCount) {
-        cursorPosition = newValue.selection.baseOffset;
-      }
-    }
+    final int newCursorPos = _calculateCursorPosition(finalText, newValue.selection.baseOffset);
 
     return TextEditingValue(
       text: finalText,
-      selection: TextSelection.collapsed(offset: cursorPosition),
+      selection: TextSelection.collapsed(offset: newCursorPos),
     );
+  }
+
+  String _formatText(String text) {
+    if (text.isEmpty) return "";
+
+    final String cleanedText = text.toEnglishNumber().replaceAll(RegExp(r"[^\d.]"), "");
+
+    final List<String> parts = cleanedText.split(".");
+    String integerPart = parts[0];
+    String decimalPart = parts.length > 1 ? parts[1] : "";
+
+    if (integerPart.length > maxDigits) {
+      integerPart = integerPart.substring(0, maxDigits);
+    }
+
+    String formattedInteger = "";
+    int count = 0;
+
+    for (int i = integerPart.length - 1; i >= 0; i--) {
+      formattedInteger = integerPart[i] + formattedInteger;
+      count++;
+      if (count == 3 && i > 0) {
+        formattedInteger = thousandSeparator + formattedInteger;
+        count = 0;
+      }
+    }
+
+    String finalText = formattedInteger;
+    if (decimalPart.isNotEmpty) {
+      if (decimalPart.length > 2) {
+        decimalPart = decimalPart.substring(0, 2);
+      }
+      finalText = "$formattedInteger$decimalSeparator$decimalPart";
+    }
+
+    return finalText;
+  }
+
+  int _calculateCursorPosition(String formattedText, int oldCursorPos) {
+    if (formattedText.isEmpty) return 0;
+
+    int separatorsBeforeCursor = 0;
+    for (int i = 0; i < formattedText.length && i < oldCursorPos; i++) {
+      if (formattedText[i] == thousandSeparator || formattedText[i] == decimalSeparator) {
+        separatorsBeforeCursor++;
+      }
+    }
+
+    int newCursorPos = oldCursorPos + separatorsBeforeCursor;
+
+    newCursorPos = newCursorPos.clamp(0, formattedText.length);
+
+    if (newCursorPos < formattedText.length && (formattedText[newCursorPos] == thousandSeparator || formattedText[newCursorPos] == decimalSeparator)) {
+      newCursorPos++;
+    }
+
+    return newCursorPos;
   }
 }
