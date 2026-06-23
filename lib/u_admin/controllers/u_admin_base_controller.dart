63d2a17b61507a7121ae193862cd6a867a430cae
Sina MohammadZadeh <sina75mn@outlook.com>
2026-06-23 23:39:11 +0330

    :)

--- lib/u_admin/controllers/u_admin_base_controller.dart
+++ lib/u_admin/controllers/u_admin_base_controller.dart
@@ -21,13 +21,29 @@ class UAdminBaseController {
 
   bool get isFa => ULocalStorage.getLocale() == "fa";
 
+  void setTotalPages(int totalCount) => totalPages((totalCount / pageSize).ceil().clamp(1, 1 << 30));
+
+  void firstPage() => pageNumber(1);
+
+  void setListState({required bool isEmpty}) => isEmpty ? state.emptying() : state.loaded();
+
+  void setError([String? message]) {
+    state.error();
+    if (message != null) UToast.error(message: message);
+  }
+
+  void reloadFirstPage(void Function() read) {
+    firstPage();
+    read();
+  }
+
   void okCallback(String? message, void Function() reload) {
     UToast.snackBar(message: message ?? U.s.submitted);
     reload();
   }
 
   void errorCallBack(String? message, void Function() reload) {
-    UToast.error(message: message ?? "");
+    UToast.error(message: message ?? U.s.errorSubmittingForm);
     reload();
   }
 }
