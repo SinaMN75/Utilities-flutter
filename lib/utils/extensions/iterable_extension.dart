95014e8241b4563e8baafb2384b7734e1ae968d3
Sina MohammadZadeh <sina75mn@outlook.com>
2026-06-27 09:53:06 +0330

    :)

--- lib/utils/extensions/iterable_extension.dart
+++ lib/utils/extensions/iterable_extension.dart
@@ -1,100 +1,44 @@
-extension GenericIterableExtentions<T> on Iterable<dynamic> {
-  Iterable<E> mapIndexed<E, T>(final E Function(int index, T item) f) sync* {
+extension GenericIterableExtensions<T> on Iterable<T> {
+  Iterable<E> mapIndexed<E>(final E Function(int index, T item) f) sync* {
     int index = 0;
     for (final T item in this) {
       yield f(index, item);
-      index = index + 1;
+      index++;
     }
   }
 
-  Iterable<dynamic> takeIfPossible(final int range) => range > length ? take(length) : take(range);
-
-  List<dynamic> insertFirstReturn<T>(final T item) {
-    final List<dynamic> list = toList();
-    list.insert(0, item);
-    return list;
-  }
-
-  T? getFirstIfExist() => isNullOrEmpty() ? null : first;
-
-  T? firstOrDefault({final T? defaultValue}) => isNullOrEmpty() ? defaultValue : first;
-
-  void forEachIndexed(void Function(int index, dynamic element) action) {
+  void forEachIndexed(final void Function(int index, T element) action) {
     int index = 0;
-    for (T element in this) {
-      action(index++, element);
-    }
+    for (final T element in this) action(index++, element);
   }
+
+  List<T> insertFirstReturn(final T item) => <T>[item, ...this];
+
+  T? getFirstIfExist() => isEmpty ? null : first;
+
+  T? firstOrDefault({final T? defaultValue}) => isEmpty ? defaultValue : first;
+
+  Iterable<T> takeIfPossible(final int range) => take(range > length ? length : range);
+
+  bool containsAll(final Iterable<T> list) => toSet().containsAll(list);
+
+  bool containsAny(final Iterable<T> list) => list.any(contains);
+
+  List<T> alternative(final T main, final T replace) => toList()
+    ..remove(main)
+    ..add(replace);
+
+  List<T> addAndReturn(final T t) => <T>[...this, t];
+
+  List<T> addAllAndReturn(final Iterable<T> t) => <T>[...this, ...t];
+
+  List<T> insertAndReturn(final int index, final T t) => toList()..insert(index, t);
 }
 
-extension IterableExtentions<T> on Iterable<T> {
-  Iterable<T> takeIfPossible(final int range) {
-    if (range > length) {
-      return take(length);
-    } else {
-      return take(range);
-    }
-  }
+extension NullableIterableExtensions<T> on Iterable<T>? {
+  bool isNullOrEmpty() => this == null || this!.isEmpty;
 
-  T? getFirstIfExist() {
-    if (isNullOrEmpty()) {
-      return null;
-    } else {
-      return first;
-    }
-  }
+  bool isNotNullOrEmpty() => !isNullOrEmpty();
 
-  List<T> alternative(final T main, final T replace) {
-    final List<T> list = toList();
-    list.remove(main);
-    list.add(replace);
-    return list;
-  }
-
-  bool containsAll<T>(final List<T> list) => toSet().containsAll(list);
-
-  bool containsAny<T>(final List<T> list) {
-    final Set<T> setA = Set<T>.of(list);
-    return setA.any(contains);
-  }
-
-  List<T> addAndReturn(final T t) {
-    final List<T> list = toList();
-    list.add(t);
-    return list;
-  }
-
-  List<T> addAllAndReturn(final List<T> t) {
-    final List<T> list = toList();
-    list.addAll(t);
-    return list;
-  }
-
-  List<T> insertAndReturn(final int index, final T t) {
-    final List<T> list = toList();
-    list.insert(index, t);
-    return list;
-  }
-}
-
-extension NullableIterableExtentions on Iterable<dynamic>? {
-  bool isNullOrEmpty() {
-    if (this == null) {
-      return true;
-    } else if (this!.isEmpty) {
-      return true;
-    }
-    return false;
-  }
-
-  bool isNotNullOrEmpty() {
-    if (this == null) {
-      return false;
-    } else if (this!.isEmpty) {
-      return false;
-    }
-    return true;
-  }
-
-  bool containsAll<T>(final List<T> list) => (this ?? <T>[]).toSet().containsAll(list);
+  bool containsAll(final Iterable<T> list) => (this ?? const <Never>[]).toSet().containsAll(list);
 }
