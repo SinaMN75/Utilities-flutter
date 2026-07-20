0421ee280a12f28ff4daa3b4ae9b19e23edaeecf
Sina MohammadZadeh <sina75mn@outlook.com>
2026-07-20 06:52:25 +0330

    :)

--- lib/data/services/auth_service.dart
+++ lib/data/services/auth_service.dart
@@ -182,4 +182,35 @@ class AuthService {
     );
     return result;
   }
+
+  Future<(UResponse<ULoginResponse>?, UEmptyResponse?, String?)> loginOrRegister({
+    required final URegisterParams p,
+    final Function(UResponse<ULoginResponse> r)? onOk,
+    final Function(UEmptyResponse e)? onError,
+    final Function(String e)? onException,
+  }) async {
+    (UResponse<ULoginResponse>?, UEmptyResponse?, String?) result = (null, null, null);
+    await UHttpClient.send(
+      method: "POST",
+      endpoint: "${U.baseUrl}/auth/LoginOrRegister",
+      body: p.toMap().add("apiKey", U.apiKey).add("token", ULocalStorage.getToken()),
+      onSuccess: (final Response r) {
+        final UResponse<ULoginResponse> response = UResponse<ULoginResponse>.fromJson(r.body, (final dynamic i) => ULoginResponse.fromMap(i));
+        ULocalStorage.setUserId(response.result!.user.id);
+        ULocalStorage.setToken(response.result!.token);
+        result = (response, null, null);
+        onOk?.call(response);
+      },
+      onError: (final Response r) {
+        final UEmptyResponse err = UEmptyResponse.fromJson(r.body);
+        result = (null, err, null);
+        onError?.call(err);
+      },
+      onException: (final String e) {
+        result = (null, null, e);
+        onException?.call(e);
+      },
+    );
+    return result;
+  }
 }
